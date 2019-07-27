44d511c029129307f7379dc06e4b352aa00747cf
FxWolfxe <ndmuench@comcast.net>
2019-07-27 17:27:42 -0400

    Changed behavior of Hediff_Morph and HediffGiver_Tf so Hediff_Morph now controls when the full transformation is attempted, fixing issue where HediffGiver_Tf was never being reset resulting in pawns never transforming. Also changed HediffGive_tf so that an override probability can be given to it in the hediff def xml file, rather then always using the mod settings (which is the default)

--- Source/Pawnmorphs/Esoteria/HediffGiver_TF.cs
+++ Source/Pawnmorphs/Esoteria/HediffGiver_TF.cs
@@ -1,31 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Verse;
+﻿using System.Collections.Generic;
 using RimWorld;
+using Verse;
 
 namespace Pawnmorph
 {
     public class HediffGiver_TF : HediffGiver
     {
+        //[Unsaved] private readonly Dictionary<Pawn, bool> _sawDict = new Dictionary<Pawn, bool>();
+
         public List<PawnKindDef> pawnkinds; // The pawnKind of the animal to be transformed into.
         public TaleDef tale; // Tale to add to the tales.
-        public TFGender forceGender = TFGender.Original; // The gender that will be forced (i.e. a ChookMorph will be forced female).
-        public float forceGenderChance = 50f; // If forceGender is provided, this is the chance the gender will be forced.
-        private bool triggered = false; // A flag to prevent us from checking endlessly.
 
-        public override void OnIntervalPassed(Pawn pawn, Hediff cause)
-        // Whenever the timer expires.
+        public TFGender
+            forceGender = TFGender.Original; // The gender that will be forced (i.e. a ChookMorph will be forced female).
+
+        public float forceGenderChance = 50f; // If forceGender is provided, this is the chance the gender will be forced.
+
+        private float changeChance = -1; 
+
+        //[Unsaved] private bool triggered = false; // A flag to prevent us from checking endlessly.
+        //not one to one, instances of this class are pooled!!!!
+
+
+        public bool TryTf(Pawn pawn, Hediff cause)
         {
-            if (!triggered && Rand.RangeInclusive(0, 100) <= LoadedModManager.GetMod<PawnmorpherMod>().GetSettings<PawnmorpherSettings>().transformChance)
+
+            float chance = changeChance < 0 //if changeChance wasn't overriden use the default from the settings 
+                ? LoadedModManager.GetMod<PawnmorpherMod>().GetSettings<PawnmorpherSettings>().transformChance
+                : changeChance;
+            
+            
+
+            if (Rand.RangeInclusive(0, 100) <= chance)
             {
                 TransformerUtility.Transform(pawn, cause, hediff, pawnkinds, tale, forceGender, forceGenderChance);
+                return true; 
             }
-            else
-            {
-                triggered = true;
-            }
+
+            return false; 
+
+
+        }
+
+        public override void OnIntervalPassed(Pawn pawn, Hediff cause)
+
+        {
+            //if (!triggered
+            // && Rand.RangeInclusive(0, 100)
+            // <= LoadedModManager.GetMod<PawnmorpherMod>().GetSettings<PawnmorpherSettings>().transformChance)
+            //    TransformerUtility.Transform(pawn, cause, hediff, pawnkinds, tale, forceGender, forceGenderChance);
+            //else
+            //    triggered = true;
+
+            //_sawDict[pawn] = true;
+            //empty 
         }
     }
 }
--- Source/Pawnmorphs/Esoteria/Hediff_Morph.cs
+++ Source/Pawnmorphs/Esoteria/Hediff_Morph.cs
@@ -10,5 +10,29 @@ namespace Pawnmorph
 {
     class Hediff_Morph : HediffWithComps
     {
+        private int _lastStage = -1;
+        public override void PostTick()
+        {
+            base.PostTick();
+
+            if (_lastStage != CurStageIndex)
+            {
+
+                _lastStage = CurStageIndex;
+
+
+                if (CurStage.hediffGivers == null) return; 
+
+                foreach (HediffGiver_TF tfGiver in CurStage.hediffGivers.OfType<HediffGiver_TF>())
+                {
+
+                    if(tfGiver.TryTf(pawn, this)) break; //try each one, one by one. break at first one that succeeds  
+
+
+                }
+
+
+            }
+        }
     }
 }
