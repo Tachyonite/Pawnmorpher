533fc563843d7077b0a8ac88723a24e812c213dc
FxWolfxe <ndmuench@comcast.net>
2019-08-05 07:45:01 -0400

    Added Chook,Boomalope,and Cow MorphDefs as well as fixed xml error in RatMorph Hediff

---
+++ Source/Pawnmorphs/Esoteria/TaleDefOf.cs
@@ -0,0 +1,16 @@
+﻿// TaleDefOf.cs modified by Iron Wolf for Pawnmorph on 08/04/2019 7:00 PM
+// last updated 08/04/2019  7:00 PM
+
+using RimWorld;
+
+namespace Pawnmorph
+{
+    /// <summary>
+    /// static container containing misc tales 
+    /// </summary>
+    [DefOf]
+    public static class TaleDefOf
+    {
+        public static TaleDef TransformationMisc; 
+    }
+}
