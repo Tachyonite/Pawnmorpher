54d9cb7181484e4fe1d79f1c2d931b83124fa4c0
FxWolfxe <ndmuench@comcast.net>
2019-08-07 11:24:03 -0400

    Manually Merged Most changes from branch balance-changes
    Merged most changes from Pheonix's branch , including segregating parts,
    morphs and thoughts, and replacing the old HediffGivers with the new
    HediffGiver_Mutation

---
+++ Source/Pawnmorphs/Esoteria/HediffGiver_Mutation.cs
@@ -0,0 +1,45 @@
+﻿// HediffGiver_Mutation.cs modified by Iron Wolf for Pawnmorph on 08/07/2019 10:19 AM
+// last updated 08/07/2019  10:19 AM
+
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace Pawnmorph
+{
+    public class HediffGiver_Mutation : HediffGiver
+    {
+        public float mtbDays;
+        public Gender gender;
+        public int chance = 100;
+        public TaleDef tale;
+        private Dictionary<Hediff, bool> triggered = new Dictionary<Hediff, bool>();
+
+        public new bool TryApply(Pawn pawn, List<Hediff> outAddedHediffs = null)
+        {
+            return PawnmorphHediffGiverUtility.TryApply(pawn, hediff, partsToAffect, canAffectAnyLivePart, countToAffect, outAddedHediffs);
+        }
+        public override void OnIntervalPassed(Pawn pawn, Hediff cause)
+        {
+            if (Rand.MTBEventOccurs(mtbDays, 60000f, 60f) && pawn.RaceProps.intelligence == Intelligence.Humanlike)
+            {
+                if ((gender == pawn.gender || (!triggered.TryGetValue(cause, false) && Rand.RangeInclusive(0, 100) <= chance)) && TryApply(pawn))
+                {
+                    IntermittentMagicSprayer.ThrowMagicPuffDown(pawn.Position.ToVector3(), pawn.MapHeld);
+                    if (cause.def.HasComp(typeof(HediffComp_Single)))
+                    {
+                        pawn.health.RemoveHediff(cause);
+                    }
+                    if (tale != null)
+                    {
+                        TaleRecorder.RecordTale(tale, new object[] { pawn });
+                    }
+                }
+                else
+                {
+                    triggered[cause] = true;
+                }
+            }
+        }
+    }
+}
