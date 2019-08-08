499b4c3e5b3964b08149a51ec03a637925bc8a4e
Brennen Cramer <48333768+BrennenCramer@users.noreply.github.com>
2019-08-07 22:35:43 -0700

    Manually merged dev and balance-changes

--- Source/Pawnmorphs/Esoteria/MutagenicStone.cs
+++ Source/Pawnmorphs/Esoteria/MutagenicStone.cs
@@ -13,40 +13,35 @@ namespace Pawnmorph
 {
     public class MutagenicStone : Mineable
     {
-
         private List<Pawn> touchingPawns = new List<Pawn>();
-
         public static HediffDef hediff = HediffDef.Named("FullRandomTF");
                 
         public override void TickRare()
         {
-
-            if (base.Spawned && LoadedModManager.GetMod<PawnmorpherMod>().GetSettings<PawnmorpherSettings>().enableMutagenMeteor)
+            if (Spawned && LoadedModManager.GetMod<PawnmorpherMod>().GetSettings<PawnmorpherSettings>().enableMutagenMeteor)
             {
-                IEnumerable<Verse.Thing> enumerable = GenRadial.RadialDistinctThingsAround(base.Position, base.Map, 2.0f, true);
+                IEnumerable<Verse.Thing> enumerable = GenRadial.RadialDistinctThingsAround(Position, Map, 2.0f, true);
                 List<Thing> thingList = enumerable.ToList();
-
 
                 for (int i = 0; i < thingList.Count; i++)
                 {
                     Pawn pawn = thingList[i] as Pawn;
                     if (pawn != null && !this.touchingPawns.Contains(pawn))
                     {
-                        this.touchingPawns.Add(pawn);
+                        touchingPawns.Add(pawn);
                     }
                 }
-                for (int j = 0; j < this.touchingPawns.Count; j++)
+                for (int j = 0; j < touchingPawns.Count; j++)
                 {
-                    Pawn pawn2 = this.touchingPawns[j];
+                    Pawn pawn2 = touchingPawns[j];
                     if (!pawn2.Spawned || !thingList.Contains(pawn2))
                     {
-                        this.touchingPawns.Remove(pawn2);
+                        touchingPawns.Remove(pawn2);
                         var targetHediff = pawn2.health.hediffSet.hediffs.FirstOrDefault(x => x.def == hediff); //need to use FirstOrDefault, First throws InvalidOperationException when run on an empty enumerable
                         if ( targetHediff != null) 
                         {
                             pawn2.health.RemoveHediff(targetHediff);
                         }
-                        
                     }
                     else
                     {
@@ -54,12 +49,9 @@ namespace Pawnmorph
                         {
                             pawn2.health.AddHediff(hediff);
                             IntermittentMagicSprayer.ThrowMagicPuffDown(pawn2.Position.ToVector3(), pawn2.Map);
-
                         }
                     }
-
                 }
-
             }
         }
     }
