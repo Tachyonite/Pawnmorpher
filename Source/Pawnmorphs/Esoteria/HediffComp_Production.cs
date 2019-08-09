dca40bebdb5676057eccd5a9c4843b58f8e385d2
FxWolfxe <ndmuench@comcast.net>
2019-08-09 12:21:12 -0400

    Fixed bug in HediffComp_Production causing null reference exception

--- Source/Pawnmorphs/Esoteria/HediffComp_Production.cs
+++ Source/Pawnmorphs/Esoteria/HediffComp_Production.cs
@@ -10,24 +10,31 @@ namespace Pawnmorph
         private float brokenChance = 0f;
         private float bondChance = 0f;
 
-        public HediffCompProperties_Production Props
-        {
-            get
-            {
-                return (HediffCompProperties_Production)this.props;
-            }
-        }
+        public HediffCompProperties_Production Props => (HediffCompProperties_Production) props;
 
         public override void CompPostTick(ref float severityAdjustment)
         {
+            ThingDef rareResource;
+
             if (Props.stages != null)
             {
                 HediffComp_Staged stage = Props.stages.ElementAt(parent.CurStageIndex);
-                Produce(stage.daysToProduce, stage.amount, stage.chance, ThingDef.Named(stage.resource), ThingDef.Named(stage.rareResource), stage.thought);
+                if (!string.IsNullOrEmpty(stage.rareResource))
+                    rareResource = ThingDef.Named(stage.rareResource);
+                else
+                    rareResource = null;
+                Produce(stage.daysToProduce, stage.amount, stage.chance, ThingDef.Named(stage.resource), rareResource,
+                        stage.thought);
             }
             else
             {
-                Produce(Props.daysToProduce, Props.amount, Props.chance, ThingDef.Named(Props.resource), ThingDef.Named(Props.rareResource));
+                if (!string.IsNullOrEmpty(Props.rareResource))
+                    rareResource = ThingDef.Named(Props.rareResource);
+                else
+                    rareResource = null;
+
+
+                Produce(Props.daysToProduce, Props.amount, Props.chance, ThingDef.Named(Props.resource), rareResource);
             }
         }
 
@@ -39,30 +46,25 @@ namespace Pawnmorph
             base.CompExposeData();
         }
 
-        public void Produce(float daysToProduce, int amount, float chance, ThingDef resource, ThingDef rareResource, ThoughtDef stageThought = null)
+        public void Produce(float daysToProduce, int amount, float chance, ThingDef resource, ThingDef rareResource,
+                            ThoughtDef stageThought = null)
         {
             MemoryThoughtHandler thoughts = Pawn.needs.mood.thoughts.memories;
             bool hasEtherBond = Pawn.health.hediffSet.HasHediff(HediffDef.Named("EtherBond"));
             bool hasEtherBroken = Pawn.health.hediffSet.HasHediff(HediffDef.Named("EtherBroken"));
 
-            if (HatchingTicker < (daysToProduce * 60000))
+            if (HatchingTicker < daysToProduce * 60000)
             {
                 HatchingTicker += 1;
             }
             else if (Pawn.Map != null)
             {
                 HatchingTicker = 0;
-                for (int i = 0; i < amount; i++)
-                {
-                    if (Rand.RangeInclusive(0, 100) <= (chance) && rareResource != null)
-                    {
+                for (var i = 0; i < amount; i++)
+                    if (Rand.RangeInclusive(0, 100) <= chance && rareResource != null)
                         GenSpawn.Spawn(rareResource, Pawn.Position, Pawn.Map);
-                    }
                     else
-                    {
-                        GenSpawn.Spawn(resource, Pawn.Position, Pawn.Map); 
-                    }
-                }
+                        GenSpawn.Spawn(resource, Pawn.Position, Pawn.Map);
 
                 if (!hasEtherBond && !hasEtherBroken)
                 {
@@ -70,24 +72,21 @@ namespace Pawnmorph
                     {
                         Pawn.health.AddHediff(HediffDef.Named("EtherBond"));
                         Find.LetterStack.ReceiveLetter(
-                            "LetterHediffFromEtherBondLabel".Translate(Pawn).CapitalizeFirst(),
-                            "LetterHediffFromEtherBond".Translate(Pawn).CapitalizeFirst(),
-                            LetterDefOf.NeutralEvent, Pawn, null, null);
+                                                       "LetterHediffFromEtherBondLabel".Translate(Pawn).CapitalizeFirst(),
+                                                       "LetterHediffFromEtherBond".Translate(Pawn).CapitalizeFirst(),
+                                                       LetterDefOf.NeutralEvent, Pawn);
                     }
                     else if (Rand.RangeInclusive(0, 100) <= brokenChance)
                     {
                         Pawn.health.AddHediff(HediffDef.Named("EtherBroken"));
                         Find.LetterStack.ReceiveLetter(
-                            "LetterHediffFromEtherBrokenLabel".Translate(Pawn).CapitalizeFirst(),
-                            "LetterHediffFromEtherBroken".Translate(Pawn).CapitalizeFirst(),
-                            LetterDefOf.NeutralEvent, Pawn, null, null);
+                                                       "LetterHediffFromEtherBrokenLabel".Translate(Pawn).CapitalizeFirst(),
+                                                       "LetterHediffFromEtherBroken".Translate(Pawn).CapitalizeFirst(),
+                                                       LetterDefOf.NeutralEvent, Pawn);
                     }
                 }
 
-                if (stageThought != null)
-                {
-                    thoughts.TryGainMemory(stageThought);
-                }
+                if (stageThought != null) thoughts.TryGainMemory(stageThought);
 
                 if (hasEtherBond && Props.etherBondThought != null)
                 {
@@ -100,13 +99,8 @@ namespace Pawnmorph
                 else
                 {
                     if (Props.genderAversion == Pawn.gender && Props.wrongGenderThought != null)
-                    {
                         thoughts.TryGainMemory(Props.wrongGenderThought);
-                    }
-                    else if (Props.thought != null)
-                    {
-                        thoughts.TryGainMemory(Props.thought);
-                    }
+                    else if (Props.thought != null) thoughts.TryGainMemory(Props.thought);
                     brokenChance += 0.5f;
                     bondChance += 0.2f;
                 }
