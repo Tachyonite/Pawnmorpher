38880c44ab6a3c34057cf70b6d59555062d8ac54
FxWolfxe <ndmuench@comcast.net>
2019-08-29 15:38:53 -0400

    created a setting to enable/disable mutagenic fallout

---
+++ Source/Pawnmorphs/Esoteria/PMIncidentDefOf.cs
@@ -0,0 +1,25 @@
+﻿// PMIncidentDefOf.cs modified by Iron Wolf for Pawnmorph on 08/29/2019 3:34 PM
+// last updated 08/29/2019  3:34 PM
+
+using RimWorld;
+
+namespace Pawnmorph
+{
+    /// <summary>
+    /// static container for incident defs
+    /// </summary>
+    [DefOf]
+    public static class PMIncidentDefOf
+    {
+        static PMIncidentDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(IncidentDef));
+        }
+
+        public static IncidentDef MutagenicShipPartCrash;
+        public static IncidentDef Disease_Cowflu;
+        public static IncidentDef Disease_Foxflu;
+        public static IncidentDef Disease_Chookflu;
+        public static IncidentDef MutagenicFallout; 
+    }
+}
