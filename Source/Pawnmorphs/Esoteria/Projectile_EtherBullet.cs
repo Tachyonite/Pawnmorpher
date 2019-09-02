0ec9c6961000e56cb58ac835f417bf4f4bcac870
FxWolfxe <ndmuench@comcast.net>
2019-09-02 08:59:17 -0400

    changed the way the tagging rifle checks if an animal can be added to the chamber database, making sure only animals that are not chaomorphs get added

--- Source/Pawnmorphs/Esoteria/Projectile_EtherBullet.cs
+++ Source/Pawnmorphs/Esoteria/Projectile_EtherBullet.cs
@@ -1,30 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Verse;
+﻿using Pawnmorph;
+using Pawnmorph.Chambers;
 using RimWorld;
-using Pawnmorph;
+using Verse;
 
 namespace EtherGun
 {
     public class Projectile_EtherBullet : Bullet
     {
         #region Properties
-        public ThingDef_EtherBullet Def
-        {
-            get
-            {
-                //Case sensitive! If you use Def it will return Def, which is this getter. This will cause a never ending cycle and a stack overflow.
-                return this.def as ThingDef_EtherBullet;
-            }
-        }
+
+        public ThingDef_EtherBullet Def => def as ThingDef_EtherBullet;
+
         #endregion
 
         #region Overrides
+
         protected override void Impact(Thing hitThing)
         {
-
             base.Impact(hitThing);
             Pawn hitPawn;
             if (Def != null && hitThing != null && hitThing is Pawn)
@@ -33,12 +25,11 @@ namespace EtherGun
 
                 if (!Def.CanAddHediffToPawn(hitPawn)) return; //if the hediff can't be added to the hit pawn just abort 
 
-                var rand = Rand.Value; // This is a random percentage between 0% and 100%
+                float rand = Rand.Value; // This is a random percentage between 0% and 100%
                 if (rand <= Def.AddHediffChance) // If the percentage falls under the chance, success!
                 {
-
                     //This checks to see if the character has a heal differential, or hediff on them already.
-                    var etherOnPawn = hitPawn.health?.hediffSet?.GetFirstHediffOfDef(Def.HediffToAdd);
+                    Hediff etherOnPawn = hitPawn.health?.hediffSet?.GetFirstHediffOfDef(Def.HediffToAdd);
                     var randomSeverity = 1f;
                     if (etherOnPawn == null)
                     {
@@ -47,58 +38,54 @@ namespace EtherGun
                         Hediff hediff = HediffMaker.MakeHediff(Def.HediffToAdd, hitPawn);
                         hediff.Severity = randomSeverity;
                         hitPawn.health.AddHediff(hediff);
-                        IntermittentMagicSprayer.ThrowMagicPuffDown(hitPawn.Position.ToVector3(), base.Map);
+                        IntermittentMagicSprayer.ThrowMagicPuffDown(hitPawn.Position.ToVector3(), Map);
                     }
-                }
-                else //failure!
-                {
                 }
             }
         }
+
         #endregion Overrides
     }
 
     public class Projectile_TaggingBullet : Bullet
     {
         #region Properties
-        public ThingDef_TaggingBullet Def
-        {
-            get
-            {
-                //Case sensitive! If you use Def it will return Def, which is this getter. This will cause a never ending cycle and a stack overflow.
-                return this.def as ThingDef_TaggingBullet;
-            }
-        }
+
+        public ThingDef_TaggingBullet Def => def as ThingDef_TaggingBullet;
+
         #endregion
 
         #region Overrides
+
         protected override void Impact(Thing hitThing)
         {
-
             base.Impact(hitThing);
-            Pawn hitPawn;
-            PawnmorphGameComp pgc = Find.World.GetComponent<PawnmorphGameComp>();
-            if (hitThing != null && hitThing is Pawn)
+            var pgc = Find.World.GetComponent<PawnmorphGameComp>();
+            if (hitThing != null && hitThing is Pawn pawn)
             {
+                Pawn hitPawn = pawn;
 
-                hitPawn = hitThing as Pawn;
-                if (hitPawn.RaceProps.intelligence != Intelligence.Humanlike && hitPawn.RaceProps.intelligence != Intelligence.ToolUser && !hitPawn.KindLabel.ToLower().StartsWith("chao"))
+                if (hitPawn.def.IsValidAnimal())
                 {
                     if (pgc.taggedAnimals.Contains(hitPawn.kindDef))
                     {
-                        Messages.Message("{0} already in genetic database".Formatted(hitPawn.kindDef.LabelCap), MessageTypeDefOf.RejectInput);
+                        Messages.Message("{0} already in genetic database".Formatted(hitPawn.kindDef.LabelCap),
+                                         MessageTypeDefOf.RejectInput);
                         return;
                     }
+
                     pgc.tagPawn(hitPawn.kindDef);
-                    Messages.Message("{0} added to database".Formatted(hitPawn.kindDef.LabelCap), MessageTypeDefOf.TaskCompletion);
+                    Messages.Message("{0} added to database".Formatted(hitPawn.kindDef.LabelCap),
+                                     MessageTypeDefOf.TaskCompletion);
                 }
-                if (hitPawn.KindLabel.ToLower().StartsWith("chao"))
+                else if (DatabaseUtilities.IsChao(hitPawn.def))
                 {
-                    Messages.Message("{0} is too genetically corrupted to be added".Formatted(hitPawn.kindDef.LabelCap), MessageTypeDefOf.RejectInput);
+                    Messages.Message("{0} is too genetically corrupted to be added".Formatted(hitPawn.kindDef.LabelCap),
+                                     MessageTypeDefOf.RejectInput);
                 }
             }
         }
+
         #endregion Overrides
     }
-
 }
