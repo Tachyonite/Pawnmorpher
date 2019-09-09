c02b5be136820a39c17a03a01a704620fea3c62d
FxWolfxe <ndmuench@comcast.net>
2019-09-09 15:30:47 -0400

    work on morph groups

---
+++ Source/Pawnmorphs/Esoteria/MorphGroupDef.cs
@@ -0,0 +1,16 @@
+﻿// MorphGroupDef.cs created by Nick M(Iron Wolf) for Blue Moon (Pawnmorph) on 09/09/2019 9:07 AM
+// last updated 09/09/2019  9:07 AM
+
+using Verse;
+
+namespace Pawnmorph
+{
+    /// <summary>
+    /// def for 'morph groups' i
+    /// ie packs,herds, ect  
+    /// </summary>
+    public class MorphGroupDef : Def 
+    {
+        
+    }
+}
---
+++ Source/Pawnmorphs/Esoteria/MorphTracker.cs
@@ -0,0 +1,53 @@
+﻿// MorphTracker.cs created by Nick M(Iron Wolf) for Blue Moon (Pawnmorph) on 09/09/2019 9:03 AM
+// last updated 09/09/2019  9:03 AM
+
+using System;
+using Verse;
+
+namespace Pawnmorph
+{
+    /// <summary>
+    /// map component for tracking morphs by type & group on a map 
+    /// </summary>
+    public class MorphTracker : MapComponent
+    {
+        public delegate void MorphCountChangedHandle(MorphTracker sender, MorphDef morph);
+
+        /// <summary>
+        /// event that is raised every time the morph count on the attached map changes
+        /// </summary>
+        public event MorphCountChangedHandle MorphCountChanged; 
+
+        public MorphTracker(Map map) : base(map)
+        {
+        }
+
+        /// <summary>
+        ///     notify this tracker that the pawn has spawned
+        /// </summary>
+        /// <param name="pawn"></param>
+        public void NotifySpawned(Pawn pawn)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// notify the map that the pawn has despawned from the map 
+        /// </summary>
+        /// <param name="pawn"></param>
+        public void NotifyDespawned(Pawn pawn)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// notify this tracker that the pawn race has changed 
+        /// </summary>
+        /// <param name="pawn"></param>
+        public void NotifyPawnRaceChanged(Pawn pawn)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
