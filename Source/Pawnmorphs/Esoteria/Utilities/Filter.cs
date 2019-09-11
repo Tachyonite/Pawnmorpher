7d0aed2903f527a043efdd1b3a5253b78f6024ae
FxWolfxe <ndmuench@comcast.net>
2019-09-11 15:16:26 -0400

    created special Ingestion Doer for use in the booster pills to replace the boosed production hediffs

---
+++ Source/Pawnmorphs/Esoteria/Utilities/Filter.cs
@@ -0,0 +1,25 @@
+﻿// Filter.cs created by Nick M(Iron Wolf) for Blue Moon (Pawnmorph) on 09/11/2019 2:56 PM
+// last updated 09/11/2019  2:56 PM
+
+using System.Collections.Generic;
+
+namespace Pawnmorph.Utilities
+{
+    /// <summary>
+    /// generic class for a filter 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Filter<T>
+    {
+        public List<T> filterList;
+        public bool isBlackList;
+
+        public bool PassesFilter(T elem)
+        {
+            var contains = filterList?.Contains(elem) ?? false;
+            if (isBlackList) return !contains;
+            return contains; 
+        }
+
+    }
+}
