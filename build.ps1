4bfd79ee103750b60243ec1e7d545652ea0262cb
FxWolfxe <ndmuench@comcast.net>
2019-09-15 15:07:46 -0400

    updated build script to exit on build error

--- build.ps1
+++ build.ps1
@@ -20,8 +20,10 @@ If(Test-Path "$buildDir/$buildName")
     Remove-Item -Path "$buildDir/$buildName" -Force
 }
 
-. "C:\Program Files (x86)\Microsoft Visual Studio\$VSVersion\Community\MSBuild\Current\Bin\MSBuild.exe"  "Source/Pawnmorphs/Pawnmorph.sln" /t:Rebuild /p:Configuration=Debug /p:Platform="any cpu" 
-
+If(!(. "C:\Program Files (x86)\Microsoft Visual Studio\$VSVersion\Community\MSBuild\Current\Bin\MSBuild.exe"  "Source/Pawnmorphs/Pawnmorph.sln" /t:Rebuild /p:Configuration=Debug /p:Platform="any cpu")) 
+{
+    exit 1 
+}
 
 Copy-Item -Path Defs, About,Assemblies,Languages,Patches, Source,Textures -Destination "$buildDir/Tmp" -Recurse
 Compress-Archive -Path "$buildDir/Tmp/*" -CompressionLevel Optimal -DestinationPath "$buildDir/$buildName"
