8c2e24ca6a33f9fee100a7e47678e346d2300d80
Micah <micah@tachyonnz.com>
2019-09-16 22:51:10 +1200

    Attempted to add some sort of framework for getting the hediffs a pawnkind desires, but gave up

--- Source/Pawnmorphs/Esoteria/MorphPawnKindExtension.cs
+++ Source/Pawnmorphs/Esoteria/MorphPawnKindExtension.cs
@@ -3,6 +3,8 @@
 
 using System.Collections.Generic;
 using Verse;
+using Multiplayer.API;
+using Pawnmorph.Utilities;
 
 namespace Pawnmorph
 {
@@ -18,5 +20,33 @@ namespace Pawnmorph
         public List<MorphCategoryDef> morphCategories = new List<MorphCategoryDef>();  
         //public List<InfluenceDef> influences; 
         public List<MutationCategoryDef> mutationCategories = new List<MutationCategoryDef>(); 
+    }
+
+    public class AddMorphsToPawn
+    {
+        public Pawn GetMorphsForPawnKind(Pawn pawn, PawnKindDef pawnKind)
+        {
+            /*
+            if (MP.IsInMultiplayer)
+            {
+                Rand.PushState(RandUtilities.MPSafeSeed);
+            }
+
+            if (pawnKind.HasModExtension<MorphPawnKindExtension>()){
+
+                MorphPawnKindExtension pKE = pawnKind.GetModExtension<MorphPawnKindExtension>();
+                IEnumerable<MorphDef> mcd = MorphCategoryDefOf.Combat.AllMorphsInCategories;
+
+                for (var i = 0; i < pKE.maxHediffs; i++)
+                {
+                    HediffDef hediff = mcd.RandomElement();
+                    
+
+                }
+
+            }
+            */
+            return pawn;
+        }
     }
 }
