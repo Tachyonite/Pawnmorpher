9353df2b4f00a992fefd9f097721afde753e9f98
FxWolfxe <ndmuench@comcast.net>
2019-09-17 09:30:58 -0400

    Updated existing reaction thoughts to take traits into account
    - created special ThoughtWorker for taking traits and hediffs into account
    - created abstract ThoughtDefs for new reaction thoughts, one for no trait, one for furries and one for body purists
    - updated existing hediff reactions to use the abstract THoughtDefs

---
+++ Source/Pawnmorphs/Esoteria/Thoughts/Worker_HediffTrait.cs
@@ -0,0 +1,59 @@
+﻿// Worker_HediffTrait.cs created by Iron Wolf for Pawnmorph on 09/17/2019 8:06 AM
+// last updated 09/17/2019  8:06 AM
+
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Pawnmorph.Thoughts
+{
+    /// <summary>
+    /// thought worker that works like ThoughtWorker_Hediff except is also respects traits
+    /// </summary>
+    public class Worker_HediffTrait: ThoughtWorker 
+
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            var firstHediff = p.health?.hediffSet?.GetFirstHediffOfDef(def.hediff); 
+            if(firstHediff?.def.stages == null) return ThoughtState.Inactive; //the target hediff must have stages 
+
+            if(!CheckTraits(p)) return ThoughtState.Inactive;
+
+            var hStageIndex = firstHediff.CurStageIndex;
+            var index = Mathf.Min(def.stages.Count - 1, hStageIndex);
+
+            return ThoughtState.ActiveAtStage(index); 
+
+        }
+
+        /// <summary>
+        /// check to make sure that the pawn's traits allow for the thought to be active 
+        /// </summary>
+        /// <param name="pawn"></param>
+        /// <returns>if traits allow the thought can be active </returns>
+        private bool CheckTraits(Pawn pawn)
+        {
+
+            var storyTraits = pawn.story?.traits;
+            if (def.nullifyingTraits != null)
+            {
+                foreach (var nullifyingTrait in def.nullifyingTraits)
+                {
+                    if (storyTraits?.HasTrait(nullifyingTrait) ?? false) return false; 
+                }
+            }
+
+            if(def.requiredTraits != null)
+            {
+                if (storyTraits == null) return false;
+                foreach (var requiredTrait in def.requiredTraits)
+                {
+                    if (!storyTraits.HasTrait(requiredTrait)) return false; 
+                }
+            }
+
+            return true; 
+        }
+    }
+}
