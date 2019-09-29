f6a6ad521d31ec33e4ae42ccdf47c3655e6d622f
FxWolfxe <ndmuench@comcast.net>
2019-09-29 17:11:36 -0400

    cut value of MorphProducts in half

---
+++ Source/Pawnmorphs/Esoteria/Aspects/MorphGroupAspect.cs
@@ -0,0 +1,15 @@
+﻿// MorphGroupAspect.cs modified by Iron Wolf for Pawnmorph on 09/29/2019 1:24 PM
+// last updated 09/29/2019  1:24 PM
+
+using Verse;
+
+namespace Pawnmorph.Aspects
+{
+    public class MorphGroupAspect : Aspect, IRaceChangeEventReceiver
+    {
+        void IRaceChangeEventReceiver.OnRaceChange(ThingDef oldRace)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
