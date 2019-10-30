5575e1e8e48964a38deaed9acb69790c873c02a2
FxWolfxe <ndmuench@comcast.net>
2019-10-30 19:02:02 -0400

    activated and tested hpatch to give mutations at pawn gen

--- Source/Pawnmorphs/Esoteria/Factions/MorphGroupMakerUtilities.cs
+++ Source/Pawnmorphs/Esoteria/Factions/MorphGroupMakerUtilities.cs
@@ -15,7 +15,8 @@ namespace Pawnmorph.Factions
         /// <summary>Applies the mutations after the pawn has been loaded by the game.</summary>
         /// <param name="pawn">The pawn.</param>
         /// <param name="canApplyRestricted">if set to <c>true</c> allow restricted mutations to be applied.</param>
-        public static void ApplyMutationsPostLoad(Pawn pawn, bool canApplyRestricted)
+        /// <param name="setAtMaxStage">if true, the hediffs added will be set to the maximum stage (familiar usually)</param>
+        public static void ApplyMutationsPostLoad(Pawn pawn, bool canApplyRestricted, bool setAtMaxStage=true)
         {
             var kindExtension = pawn.kindDef.GetModExtension<MorphPawnKindExtension>();
             if (kindExtension == null) return;
@@ -31,6 +32,7 @@ namespace Pawnmorph.Factions
 
 
             List<HediffGiver_Mutation> toGive;
+            List<Hediff> allAdded =  setAtMaxStage ?  new List<Hediff>(): null; 
 
             int toGiveCount = kindExtension.hediffRange.RandomInRange;
             if (givers.Count <= toGiveCount)
@@ -50,7 +52,22 @@ namespace Pawnmorph.Factions
                 }
             }
 
-            foreach (HediffGiver_Mutation giver in toGive) giver.TryApply(pawn, MutagenDefOf.defaultMutagen);
+            foreach (HediffGiver_Mutation giver in toGive)
+            {
+                giver.TryApply(pawn, MutagenDefOf.defaultMutagen, allAdded);
+            }
+
+            if (setAtMaxStage)
+            {
+                foreach (Hediff hediff in allAdded)
+                {
+                    if(hediff.pawn == null) continue; //sometimes the hediffs are removed by other mutations 
+                    var lastStage = hediff.def.stages?.Last(); 
+                    if(lastStage == null) continue;
+                    var severity = lastStage.minSeverity + 0.01f;
+                    hediff.Severity = severity;
+                }
+            }
 
             pawn.CheckRace(false); //don't apply missing mutations to avoid giving restricted mutations and to respect the limit
         }
--- Source/Pawnmorphs/Esoteria/HPatches/PawnGroupKindWorkerPatches.cs
+++ Source/Pawnmorphs/Esoteria/HPatches/PawnGroupKindWorkerPatches.cs
@@ -7,18 +7,18 @@ using Harmony;
 using Pawnmorph.Factions;
 using RimWorld;
 using Verse;
+#pragma warning disable 1591
 
-#pragma warning disable 01591
 namespace Pawnmorph.HPatches
 {
     public static class PawnGroupKindWorkerPatches
     {
-        /*[HarmonyPatch(typeof(PawnGroupKindWorker))]
+        [HarmonyPatch(typeof(PawnGroupKindWorker))]
         [HarmonyPatch(nameof(PawnGroupKindWorker.GeneratePawns))] //might want to patch PawnGroupKindWorker_Normal,Trader instead 
         [HarmonyPatch(new Type[]
         {
             typeof(PawnGroupMakerParms), typeof(PawnGroupMaker), typeof(bool)
-        })]*/ //disabled until later 
+        })] 
         public static class GeneratePawnsPatch
         {
             public static void Postfix(PawnGroupMakerParms parms, PawnGroupMaker groupMaker, bool errorOnZeroResults,
