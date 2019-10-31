622bbbe464494460c86dc3c48690017db3e58802
FxWolfxe <ndmuench@comcast.net>
2019-10-31 17:53:07 -0400

    no more overlapping parts at spawn

--- Source/Pawnmorphs/Esoteria/Factions/MorphGroupMakerUtilities.cs
+++ Source/Pawnmorphs/Esoteria/Factions/MorphGroupMakerUtilities.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using Verse;
 
 namespace Pawnmorph.Factions
@@ -22,6 +23,7 @@ namespace Pawnmorph.Factions
             if (kindExtension == null) return;
 
             List<HediffGiver_Mutation> givers;
+            HashSet<BodyPartDef> addedPartsSet = new HashSet<BodyPartDef>(); 
             if (canApplyRestricted)
                 givers = kindExtension.AllMutationGivers.ToList();
             else
@@ -31,29 +33,42 @@ namespace Pawnmorph.Factions
                                       .ToList();
 
 
-            List<HediffGiver_Mutation> toGive;
+
+
+
+            List<HediffGiver_Mutation> toGive = new List<HediffGiver_Mutation>(); 
             List<Hediff> allAdded =  setAtMaxStage ?  new List<Hediff>(): null; 
-
+            
+            
             int toGiveCount = kindExtension.hediffRange.RandomInRange;
-            if (givers.Count <= toGiveCount)
-            {
-                toGive = givers;
-            }
-            else
-            {
-                toGive = new List<HediffGiver_Mutation>();
-                for (var i = 0; i < toGiveCount; i++)
-                {
-                    int rIndex = Rand.Range(0, givers.Count);
-                    HediffGiver_Mutation rGiver = givers[rIndex];
-                    givers.RemoveAt(rIndex); //remove each giver so it can't be picked again
 
-                    toGive.Add(rGiver);
+
+            var max = Mathf.Min(givers.Count, toGiveCount);
+
+            for (int i = 0; i < max; i++)
+            {
+                if(givers.Count == 0) break;
+                while (true)
+                {
+                    if(givers.Count == 0) break;
+                    var rI = Rand.Range(0, givers.Count);
+                    var mGiver = givers[rI];
+
+                    givers.RemoveAt(rI); //remove the entry so we don't pull duplicates 
+                    if (mGiver.partsToAffect.Any(p => addedPartsSet.Contains(p))) //make sure its for a part we haven't encountered yet
+                    {
+                        continue;
+                    }
+
+                    toGive.Add(mGiver); 
+                    break;
                 }
+
             }
 
             foreach (HediffGiver_Mutation giver in toGive)
             {
+                Log.Message($"applying {giver.hediff.defName} to {pawn.Name}");
                 giver.TryApply(pawn, MutagenDefOf.defaultMutagen, allAdded);
             }
 
@@ -66,8 +81,11 @@ namespace Pawnmorph.Factions
                     if(lastStage == null) continue;
                     var severity = lastStage.minSeverity + 0.01f;
                     hediff.Severity = severity;
+                    
                 }
             }
+
+            
 
             pawn.CheckRace(false); //don't apply missing mutations to avoid giving restricted mutations and to respect the limit
         }
