c673a6f67ef580664212cceb0e2b83334f5569dc
FxWolfxe <ndmuench@comcast.net>
2019-12-09 18:05:17 -0500

    Added ‘InstinctEffector’ mod extension for attaching to things that will have some effect on Sapient animal’s sapience level
    InstinctEffector has 3 fields, the change in ‘instinct’, which is proportional to the sapience drop, an optional ThougthDef for adding a special thought to the pawn when their sapience drops and an optional TaleDef for recording a custom tale when the pawns sapience drops.
    Right now only MentalStateDefs work properly with this

--- Source/Pawnmorphs/Esoteria/Comp_SapientAnimal.cs
+++ Source/Pawnmorphs/Esoteria/Comp_SapientAnimal.cs
@@ -3,6 +3,8 @@
 
 using System;
 using JetBrains.Annotations;
+using Pawnmorph.DefExtensions;
+using RimWorld;
 using UnityEngine;
 using Verse;
 using Verse.AI;
@@ -10,25 +12,38 @@ using Verse.AI;
 namespace Pawnmorph
 {
     /// <summary>
-    /// component for controlling instinct and mental breaks of sapient animals 
+    ///     component for controlling instinct and mental breaks of sapient animals
     /// </summary>
     /// <seealso cref="Verse.ThingComp" />
     public class Comp_SapientAnimal : ThingComp
     {
-     
         private SapientAnimalMentalBreaker _mentalBreaker;
 
+        private int _instinctLevelRaw;
+
         /// <summary>
-        /// Gets the mental breaker.
+        ///     Gets the mental breaker.
         /// </summary>
         /// <value>
-        /// The mental breaker.
+        ///     The mental breaker.
         /// </value>
         [NotNull]
-        public SapientAnimalMentalBreaker MentalBreaker => _mentalBreaker; 
+        public SapientAnimalMentalBreaker MentalBreaker => _mentalBreaker;
+
+        /// <summary>
+        ///     Gets or sets the 'instinct level'.
+        /// </summary>
+        /// <value>
+        ///     the instinct level
+        /// </value>
+        public int InstinctLevel
+        {
+            get => _instinctLevelRaw;
+            set => _instinctLevelRaw = Mathf.Max(value, 0);
+        }
 
         [NotNull]
-        Pawn Pawn
+        private Pawn Pawn
         {
             get
             {
@@ -36,30 +51,17 @@ namespace Pawnmorph
                 {
                     var p = (Pawn) parent;
                     if (p == null) throw new ArgumentException(nameof(parent));
-                    return p; 
+                    return p;
                 }
                 catch (InvalidCastException e)
                 {
-                    throw new InvalidCastException($"on {parent.Label}",e);
+                    throw new InvalidCastException($"on {parent.Label}", e);
                 }
             }
         }
 
-        private int _instinctLevelRaw;
-
-
         /// <summary>
-        /// call this to notify the comp that the attached pawn has recovered from the given mental state 
-        /// </summary>
-        /// <param name="state">The state.</param>
-        public void Notify_RecoveredFromState([NotNull] MentalState state)
-        {
-            _mentalBreaker?.NotifyRecoveredFromMentalBreak();
-        }
-
-
-        /// <summary>
-        /// called every tick 
+        ///     called every tick
         /// </summary>
         public override void CompTick()
         {
@@ -68,36 +70,61 @@ namespace Pawnmorph
         }
 
         /// <summary>
-        /// Initializes this comp
+        ///     Handles the instinct effect.
+        /// </summary>
+        /// <param name="instinctEffect">The instinct effect.</param>
+        public void HandleInstinctEffect([NotNull] InstinctEffector instinctEffect)
+        {
+            var sapienceNeed = Pawn.needs.TryGetNeed<Need_Control>();
+            if (sapienceNeed == null)
+            {
+                Log.Error($"sapient animal {Pawn.Name?.ToStringFull ?? Pawn.LabelShort} does not have the sapience need?");
+                return;
+            }
+
+            sapienceNeed.AddInstinctChange(instinctEffect.baseInstinctOffset);
+
+            if (instinctEffect.thought != null) Pawn.TryGainMemory(instinctEffect.thought);
+            if (instinctEffect.taleDef != null) TaleRecorder.RecordTale(instinctEffect.taleDef, Pawn);
+        }
+
+        /// <summary>
+        ///     Initializes this comp
         /// </summary>
         /// <param name="props">The props.</param>
         public override void Initialize(CompProperties props)
         {
             base.Initialize(props);
-            _mentalBreaker = _mentalBreaker ?? new SapientAnimalMentalBreaker(Pawn); 
+            _mentalBreaker = _mentalBreaker ?? new SapientAnimalMentalBreaker(Pawn);
+        }
+
+
+        /// <summary>
+        ///     call this to notify the comp that the attached pawn has recovered from the given mental state
+        /// </summary>
+        /// <param name="state">The state.</param>
+        public void Notify_RecoveredFromState([NotNull] MentalState state)
+        {
+            _mentalBreaker?.NotifyRecoveredFromMentalBreak();
+
+            var instinctEffect = state.def.GetModExtension<InstinctEffector>();
+            if (instinctEffect == null)
+            {
+                Log.Warning($"mental state {state.def.defName} has no {nameof(InstinctEffector)} but is allowed on sapient animals, is this intended?");
+                return;
+            }
+
+            HandleInstinctEffect(instinctEffect);
         }
 
         /// <summary>
-        /// called to expose this instances data.
+        ///     called to expose this instances data.
         /// </summary>
         public override void PostExposeData()
         {
             base.PostExposeData();
 
-            Scribe_Deep.Look(ref _mentalBreaker, "mentalBreaker", Pawn); 
-            
-        }
-
-        /// <summary>
-        /// Gets or sets the 'instinct level'.
-        /// </summary>
-        /// <value>
-        /// the instinct level 
-        /// </value>
-        public int InstinctLevel
-        {
-            get { return _instinctLevelRaw; }
-            set { _instinctLevelRaw = Mathf.Max(value, 0); }
+            Scribe_Deep.Look(ref _mentalBreaker, "mentalBreaker", Pawn);
         }
     }
 }
---
+++ Source/Pawnmorphs/Esoteria/DefExtensions/InstinctEffector.cs
@@ -0,0 +1,30 @@
+﻿// InstinctEffector.cs modified by Iron Wolf for Pawnmorph on 12/09/2019 5:50 PM
+// last updated 12/09/2019  5:50 PM
+
+using RimWorld;
+using Verse;
+
+namespace Pawnmorph.DefExtensions
+{
+    /// <summary>
+    /// def extension to make a def affect a sapient animal's instinct/sapience level 
+    /// </summary>
+    /// <seealso cref="Verse.DefModExtension" />
+    public class InstinctEffector : DefModExtension
+    {
+        /// <summary>
+        /// The base instinct offset
+        /// </summary>
+        public int baseInstinctOffset;
+
+        /// <summary>
+        /// The thought to add to the sapient animal 
+        /// </summary>
+        public ThoughtDef thought;
+
+        /// <summary>
+        /// The tale definition to add 
+        /// </summary>
+        public TaleDef taleDef; 
+    }
+}
