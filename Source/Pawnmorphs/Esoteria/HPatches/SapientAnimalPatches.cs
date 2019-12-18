5f356fa46974e795256ce8697991b9c1e8361b63
FxWolfxe <ndmuench@comcast.net>
2019-12-17 19:31:24 -0500

    disabled mating for sapient animals

---
+++ Source/Pawnmorphs/Esoteria/HPatches/SapientAnimalPatches.cs
@@ -0,0 +1,46 @@
+﻿// SapientAnimalPatches.cs modified by Iron Wolf for Pawnmorph on 12/17/2019 7:21 PM
+// last updated 12/17/2019  7:21 PM
+
+using Harmony;
+using JetBrains.Annotations;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace Pawnmorph.HPatches
+{
+    class SapientAnimalPatches
+    {
+        [HarmonyPatch(typeof(JobGiver_Mate)), HarmonyPatch("TryGiveJob")]
+        static class DisableMattingPatch
+        {
+            [HarmonyPostfix]
+            static void DisableMattingForSA([NotNull] Pawn pawn, [CanBeNull] ref Job __result)
+            {
+                if (__result != null)
+                {
+                    if (!CanMate(pawn))
+                    {
+                        __result = null;
+                        return;
+                    }
+
+                    if (__result.targetA.Thing is Pawn oPawn)
+                    {
+                        if (!CanMate(oPawn))
+                        {
+                            __result = null;
+                            return; 
+                        }
+                    }
+                }
+
+                bool CanMate(Pawn p) //only pure animals and permanently ferals can mate
+                {
+                    var fStatus = p?.GetFormerHumanStatus();
+                    return fStatus == null || fStatus == FormerHumanStatus.PermanentlyFeral;
+                }
+            }
+        }
+    }
+}
