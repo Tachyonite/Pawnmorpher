56409b8e41d6ed0c382755bc889fc346e4467a19
FxWolfxe <ndmuench@comcast.net>
2019-12-21 10:00:14 -0500

    made sure PawnKindDefs doen't override backstories when giving mutations

--- Source/Pawnmorphs/Esoteria/HPatches/PawnGroupKindWorkerPatches.cs
+++ Source/Pawnmorphs/Esoteria/HPatches/PawnGroupKindWorkerPatches.cs
@@ -1,12 +1,15 @@
 ﻿// PawnGroupKindWorkerPatches.cs created by Iron Wolf for Pawnmorph on 10/30/2019 1:00 PM
 // last updated 10/30/2019  1:01 PM
 
-using System;
 using System.Collections.Generic;
+using System.Linq;
+using AlienRace;
 using Harmony;
 using Pawnmorph.Factions;
+using Pawnmorph.Utilities;
 using RimWorld;
 using Verse;
+
 #pragma warning disable 1591
 
 namespace Pawnmorph.HPatches
@@ -15,10 +18,10 @@ namespace Pawnmorph.HPatches
     {
         [HarmonyPatch(typeof(PawnGroupKindWorker))]
         [HarmonyPatch(nameof(PawnGroupKindWorker.GeneratePawns))] //might want to patch PawnGroupKindWorker_Normal,Trader instead 
-        [HarmonyPatch(new Type[]
+        [HarmonyPatch(new[]
         {
             typeof(PawnGroupMakerParms), typeof(PawnGroupMaker), typeof(bool)
-        })] 
+        })]
         public static class GeneratePawnsPatch
         {
             public static void Postfix(PawnGroupMakerParms parms, PawnGroupMaker groupMaker, bool errorOnZeroResults,
@@ -26,13 +29,22 @@ namespace Pawnmorph.HPatches
             {
                 if ((__result?.Count ?? 0) == 0) return;
 
+
                 foreach (Pawn pawn in __result)
                 {
-                    MorphGroupMakerUtilities.ApplyMutationsPostLoad(pawn, false); 
+                    IEnumerable<BackstoryDef> backstories = (pawn.story?.AllBackstories)
+                                                           .MakeSafe() //probably want to make this without linq for performance reasons? 
+                                                           .Select(b => DefDatabase<BackstoryDef>.GetNamed(b.identifier,
+                                                                                                           false))
+                                                           .Where(b => b
+                                                                    != null); //only alien race's backstories can add mutations 
+
+                    if (backstories.Any(b => b.GetModExtension<MorphPawnKindExtension>() != null))
+                        continue; //check if they have any backstories that apply mutations, if so don't add more 
+
+                    MorphGroupMakerUtilities.ApplyMutationsPostLoad(pawn, false);
                 }
             }
         }
-
-
     }
 }
