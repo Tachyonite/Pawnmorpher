ce717f2136fe68fcc987fa0e915871b11ba42c5a
FxWolfxe <ndmuench@comcast.net>
2019-12-21 09:06:27 -0500

    fixed former humans constantly doing recreation

--- Source/Pawnmorphs/Esoteria/Jobs/Giver_SapientAnimalJoy.cs
+++ Source/Pawnmorphs/Esoteria/Jobs/Giver_SapientAnimalJoy.cs
@@ -22,8 +22,25 @@ namespace Pawnmorph.Jobs
         /// <returns></returns>
         protected override Job TryGiveJobFromJoyGiverDefDirect(JoyGiverDef def, Pawn pawn)
         {
-            if (!def.IsValidFor(pawn)) return null; 
+            if (!def.IsValidFor(pawn)) return null;
+
+            var joyNeed = pawn.needs?.joy?.CurLevelPercentage ?? 1;
+            if (joyNeed > 0.75f) return null; //make sure we don't assign the need constantly  
+
             return base.TryGiveJobFromJoyGiverDefDirect(def, pawn);
+        }
+
+        /// <summary>
+        /// Gets the priority.
+        /// </summary>
+        /// <param name="pawn">The pawn.</param>
+        /// <returns></returns>
+        public override float GetPriority(Pawn pawn)
+        {
+
+            var joyNeed = pawn.needs?.joy?.CurLevelPercentage ?? 1;
+            if (joyNeed > 0.75f) return 0; //make sure we don't assign the need constantly  
+            return base.GetPriority(pawn);
         }
     }
 }
