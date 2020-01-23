a0c9d9df2f4dd07a0290ffa5929ccf6b459e6f57
FxWolfxe <ndmuench@comcast.net>
2020-01-22 20:08:05 -0500

    created stock generator for generating all injectors of given class

---
+++ Source/Pawnmorphs/Esoteria/PMThingCategoryDefOf.cs
@@ -0,0 +1,15 @@
+﻿// PMThingCategoryDefOf.cs modified by Iron Wolf for Pawnmorph on 01/22/2020 5:43 PM
+// last updated 01/22/2020  5:43 PM
+
+using RimWorld;
+using Verse;
+
+#pragma warning disable 1591
+namespace Pawnmorph
+{
+    [DefOf]
+    public static class PMThingCategoryDefOf
+    {
+        public static ThingCategoryDef Injector;
+    }
+}
