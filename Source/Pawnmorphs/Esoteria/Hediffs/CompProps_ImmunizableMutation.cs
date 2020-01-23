21142782a3c8545d5047c7619580c23061301f54
FxWolfxe <ndmuench@comcast.net>
2020-01-23 18:18:09 -0500

    The MutagenSensitivity Stat now affects the mutagenic diseases

---
+++ Source/Pawnmorphs/Esoteria/Hediffs/CompProps_ImmunizableMutation.cs
@@ -0,0 +1,28 @@
+﻿// CompProps_TFImmunizable.cs modified by Iron Wolf for Pawnmorph on 01/23/2020 6:07 PM
+// last updated 01/23/2020  6:07 PM
+
+using RimWorld;
+using Verse;
+#pragma warning disable 1591
+namespace Pawnmorph.Hediffs
+{
+    public class CompProps_ImmunizableMutation : HediffCompProperties_Immunizable
+    {
+        public CompProps_ImmunizableMutation()
+        {
+            compClass = typeof(Comp_ImmunizableMutation);
+        }
+    }
+
+    public class Comp_ImmunizableMutation : HediffComp_Immunizable
+    {
+        protected override float SeverityChangePerDay()
+        {
+            var mult = Pawn.GetStatValue(PMStatDefOf.MutagenSensitivity);
+            var baseVal = base.SeverityChangePerDay();
+            if (baseVal > 0) baseVal *= mult; //only have it boost the severity gain when the mutations are getting worse 
+            return baseVal; 
+            
+        }
+    }
+}
