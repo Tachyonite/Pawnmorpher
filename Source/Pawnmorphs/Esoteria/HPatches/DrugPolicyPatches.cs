49e3d6b0cf06b082bce0158ad19bc5f024f889ab
FxWolfxe <ndmuench@comcast.net>
2020-02-14 20:51:47 -0500

    some fixes to account for removal of Thing and Hediff Defs
    created some harmony patches to fix Rimworld classes so they remove invalid or missing entries post load to fix old saves

---
+++ Source/Pawnmorphs/Esoteria/HPatches/DrugPolicyPatches.cs
@@ -0,0 +1,41 @@
+﻿// DrugPolicyPatches.cs modified by Iron Wolf for Pawnmorph on 02/14/2020 8:39 PM
+// last updated 02/14/2020  8:39 PM
+
+using System.Collections.Generic;
+using Harmony;
+using JetBrains.Annotations;
+using RimWorld;
+using Verse;
+
+namespace Pawnmorph.HPatches
+{
+    static class DrugPolicyPatches
+    {
+        [HarmonyPatch(typeof(Pawn_DrugPolicyTracker))]
+        [HarmonyPatch(nameof(Pawn_DrugPolicyTracker.ExposeData))]
+        static class DrugPolicyFix
+        {
+            /// <summary>
+            /// simple fix to drug policy to account for thing defs being removed 
+            /// </summary>
+            /// <param name="___drugTakeRecords">The drug take records.</param>
+            [HarmonyPostfix]
+            static void Postfix([CanBeNull] List<DrugTakeRecord> ___drugTakeRecords)
+            {
+                if (Scribe.mode == LoadSaveMode.PostLoadInit)
+                {
+                    if (___drugTakeRecords == null) return;
+                    for (int i = ___drugTakeRecords.Count - 1; i >= 0; i--)
+                    {
+                        var record = ___drugTakeRecords[i];
+                        if (record?.drug == null) //remove any null records or records with null drugs 
+                        {
+                            ___drugTakeRecords.RemoveAt(i); 
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
