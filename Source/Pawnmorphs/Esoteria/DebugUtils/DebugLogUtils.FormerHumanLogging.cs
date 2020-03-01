3c251c967149cdd3c5a1c37209b5ddcf5f078d63
FxWolfxe <ndmuench@comcast.net>
2020-03-01 11:48:39 -0500

    started work on SapientAnimal ThinkTree Rework
    - figured out how to get sapient animals to hold still while drafted
    
    note: this adds incompatibility with AnimalLogic that needs to be addressed

---
+++ Source/Pawnmorphs/Esoteria/DebugUtils/DebugLogUtils.FormerHumanLogging.cs
@@ -0,0 +1,75 @@
+﻿// DebugLogUtils.FormerHumanLogging.cs created by Iron Wolf for Pawnmorph on //2020 
+// last updated 03/01/2020  10:28 AM
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using JetBrains.Annotations;
+using Pawnmorph.Utilities;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+
+#pragma warning disable 1591
+namespace Pawnmorph.DebugUtils
+{
+    public static partial class DebugLogUtils
+    {
+        private const string FH_CATEGORY = MAIN_CATEGORY_NAME +  "-Former Humans"; 
+
+        [DebugOutput(category = FH_CATEGORY)]
+        static void LogFormerHumanLordStatus()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (Pawn pawn in PawnsFinder.AllMaps_FreeColonists.Where(p => p.IsFormerHuman()))
+            {
+                builder.AppendLine($"{pawn.Name}-{(pawn.GetLord()?.CurLordToil?.GetType().Name).ToStringSafe()}"); 
+            }
+
+            Log.Message(builder.ToString()); 
+        }
+
+        [DebugOutput(category = FH_CATEGORY)]
+        static void PrintAnimalThinkTree()
+        {
+            var tTree = DefDatabase<ThinkTreeDef>.GetNamed("Animal");
+            if (tTree == null) return;
+
+            var outStr = TreeUtilities.PrettyPrintTree(tTree.thinkRoot, GetChildren, GetNodeLabel);
+
+            Log.Message(outStr); 
+
+        }
+
+        static IEnumerable<ThinkNode> GetChildren([NotNull] ThinkNode node)
+        {
+            if (node is ThinkNode_SubtreesByTag) return Enumerable.Empty<ThinkNode>();
+            if (node is ThinkNode_Subtree) return Enumerable.Empty<ThinkNode>();
+            return node.subNodes.MakeSafe(); 
+        }
+
+        static string GetNodeLabel([NotNull] ThinkNode node)
+        {
+            var tLabel = node.GetType().Name; 
+            if (node is ThinkNode_SubtreesByTag streeTag)
+            {
+                return tLabel + "-" + streeTag.insertTag; 
+            }
+
+            if (node is ThinkNode_Subtree sTree)
+            {
+                var fInfo = typeof(ThinkNode_Subtree).GetField("treeDef", BindingFlags.Instance | BindingFlags.NonPublic);
+                var def = (Def) fInfo.GetValue(sTree);
+
+                return tLabel + "-" + def.defName; 
+            }
+
+            return tLabel; 
+        }
+
+    }
+}
---
+++ Source/Pawnmorphs/Esoteria/ThinkNodes/ConditionalSapientAnimal.cs
@@ -0,0 +1,31 @@
+﻿// ConditionalSapientAnimal.cs created by Iron Wolf for Pawnmorph on 03/01/2020 11:00 AM
+// last updated 03/01/2020  11:00 AM
+
+using Verse;
+using Verse.AI;
+
+namespace Pawnmorph.ThinkNodes
+{
+    /// <summary>
+    /// conditional think nodes for sapient animals 
+    /// </summary>
+    /// <seealso cref="Verse.AI.ThinkNode_Conditional" />
+    public class ConditionalSapientAnimal : ThinkNode_Conditional
+    {
+        /// <summary>
+        /// if the pawn must be a colonist to 
+        /// </summary>
+        public bool mustBeColonist = true;
+        /// <summary>
+        /// checks if this think node is satisfied or not 
+        /// </summary>
+        /// <param name="pawn">The pawn.</param>
+        /// <returns></returns>
+        protected override bool Satisfied(Pawn pawn)
+        {
+            if (!pawn.IsSapientFormerHuman()) return false;
+            if (mustBeColonist) return pawn.IsColonist;
+            return true; 
+        }
+    }
+}
