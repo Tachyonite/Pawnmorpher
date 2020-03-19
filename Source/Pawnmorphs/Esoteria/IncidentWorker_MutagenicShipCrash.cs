35a05e3648fe89470cca4f451a2214a02fdc9886
FxWolfxe <ndmuench@comcast.net>
2020-03-19 13:31:50 -0400

    updated muagenic ship incident to not use obsolete function

--- Source/Pawnmorphs/Esoteria/IncidentWorker_MutagenicShipCrash.cs
+++ Source/Pawnmorphs/Esoteria/IncidentWorker_MutagenicShipCrash.cs
@@ -15,7 +15,7 @@ namespace Pawnmorph
     ///     incident worker for mutagenic ship crash
     /// </summary>
     /// <seealso cref="RimWorld.IncidentWorker" />
-    public class IncidentWorker_MutagenicShipCrash : IncidentWorker_CrashedShipPart
+    public class IncidentWorker_MutagenicShipCrash : IncidentWorker
     {
         // Field ShipPointsFactor with token 0400094F
         private const float ShipPointsFactor = 0.9f;
@@ -24,71 +24,77 @@ namespace Pawnmorph
         private const int IncidentMinimumPoints = 300;
 
         /// <summary>
-        /// Tries to execute the worker.
+        ///     Gets the count to spawn.
+        /// </summary>
+        /// <value>
+        ///     The count to spawn.
+        /// </value>
+        protected virtual int CountToSpawn => 1;
+
+        /// <summary>
+        ///     Determines whether this instance can fire now with the specified parms
+        /// </summary>
+        /// <param name="parms">The parms.</param>
+        /// <returns>
+        ///     <c>true</c> if this instance can fire now with the specified parms otherwise, <c>false</c>.
+        /// </returns>
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            return ((Map) parms.target).listerThings.ThingsOfDef(def.mechClusterBuilding).Count <= 0;
+        }
+
+        /// <summary>
+        ///     Tries to execute the worker.
         /// </summary>
         /// <param name="parms">The parms.</param>
         /// <returns></returns>
         protected override bool TryExecuteWorker(IncidentParms parms)
         {
             var map = (Map) parms.target;
-            var targetInfoList = new List<TargetInfo>();
+            var list = new List<TargetInfo>();
             ThingDef shipPartDef = def.mechClusterBuilding;
-            IntVec3 dropPodLocation = MechClusterUtility.FindDropPodLocation(map, spot =>
-            {
-                if (!spot.Fogged(map) && GenConstruct.CanBuildOnTerrain(shipPartDef, spot, map, Rot4.North))
-                    return GenConstruct.CanBuildOnTerrain(shipPartDef,
-                                                          new IntVec3(spot.x - Mathf.CeilToInt(shipPartDef.size.x / 2f), spot.y,
-                                                                      spot.z), map, Rot4.North);
-                return false;
-            });
-            if (dropPodLocation == IntVec3.Invalid)
-                return false;
-            float num = Mathf.Max(parms.points * 0.9f, 300f);
-            var genParams = new PawnGroupMakerParms
-            {
-                groupKind = PawnGroupKindDefOf.Combat,
-                tile = map.Tile,
-                faction = Faction.OfMechanoids,
-                points = num
-            };
-            List<Pawn> list = PawnGroupMakerUtility.GeneratePawns(genParams)
-                                                   .ToList();
-            Thing innerThing = ThingMaker.MakeThing(shipPartDef);
-            innerThing.SetFaction(Faction.OfMechanoids);
+            IntVec3 intVec = FindDropPodLocation(map, CanPlaceAt);
+            if (intVec == IntVec3.Invalid) return false;
+            float points = Mathf.Max(parms.points * 0.9f, 300f);
+            List<Pawn> list2 = PawnGroupMakerUtility.GeneratePawns(new PawnGroupMakerParms
+                                                     {
+                                                         groupKind = PawnGroupKindDefOf.Combat,
+                                                         tile = map.Tile,
+                                                         faction = Faction.OfMechanoids,
+                                                         points = points
+                                                     })
+                                                    .ToList();
+            Thing thing = ThingMaker.MakeThing(shipPartDef);
+            thing.SetFaction(Faction.OfMechanoids);
             LordMaker.MakeNewLord(Faction.OfMechanoids, new LordJob_SleepThenMechanoidsDefend(new List<Thing>
             {
-                innerThing
-            }, Faction.OfMechanoids, 28f, dropPodLocation, false, false), map, list);
-            DropPodUtility.DropThingsNear(dropPodLocation, map, list);
-            foreach (Thing thing in list)
-                thing.TryGetComp<CompCanBeDormant>()?.ToSleep();
-            targetInfoList.AddRange(list.Select(p => new TargetInfo(p)));
-            GenSpawn.Spawn(SkyfallerMaker.MakeSkyfaller(PMThingDefOf.CrashedMutagenicShipPartIncoming, innerThing), dropPodLocation, map);
-            targetInfoList.Add(new TargetInfo(dropPodLocation, map));
-            SendStandardLetter(parms, targetInfoList, Array.Empty<NamedArgument>());
+                thing
+            }, Faction.OfMechanoids, 28f, intVec, false, false), map, list2);
+            DropPodUtility.DropThingsNear(intVec, map, list2);
+            foreach (Pawn item in list2) item.TryGetComp<CompCanBeDormant>()?.ToSleep();
+            list.AddRange(list2.Select(p => new TargetInfo(p)));
+            GenSpawn.Spawn(SkyfallerMaker.MakeSkyfaller(PMThingDefOf.CrashedMutagenicShipPartIncoming, thing), intVec, map);
+            list.Add(new TargetInfo(intVec, map));
+            SendStandardLetter(parms, list);
             return true;
+
+            bool CanPlaceAt(IntVec3 loc)
+            {
+                if (loc.Fogged(map) || !GenAdj.OccupiedRect(loc, Rot4.North, shipPartDef.Size).InBounds(map)) return false;
+                return GenConstruct.CanBuildOnTerrain(shipPartDef, loc, map, Rot4.North);
+            }
         }
 
 
-        /// <summary>
-        /// Gets the count to spawn.
-        /// </summary>
-        /// <value>
-        /// The count to spawn.
-        /// </value>
-        protected virtual int CountToSpawn => 1;
-
-        /// <summary>
-        /// Determines whether this instance can fire now with the specified parms
-        /// </summary>
-        /// <param name="parms">The parms.</param>
-        /// <returns>
-        ///   <c>true</c> if this instance can fire now with the specified parms otherwise, <c>false</c>.
-        /// </returns>
-        protected override bool CanFireNowSub( IncidentParms parms)
+        private static IntVec3 FindDropPodLocation(Map map, Predicate<IntVec3> validator)
         {
-            return ((Map) parms.target).listerThings.ThingsOfDef(def.mechClusterBuilding).Count <= 0;
-        }
+            for (var i = 0; i < 200; i++)
+            {
+                IntVec3 intVec = RCellFinder.FindSiegePositionFrom(DropCellFinder.FindRaidDropCenterDistant(map), map);
+                if (validator(intVec)) return intVec;
+            }
 
+            return IntVec3.Invalid;
+        }
     }
 }
