e9b4fcc6d843e506f7f2b3d07c59dc8f7338ec5d
FxWolfxe <ndmuench@comcast.net>
2020-04-03 15:56:27 -0400

    initial test of conditional loading of mod integrations

---
+++ Source/Pawnmorphs/PMCompactHediffIIntegration/CHIntegration.cs
@@ -0,0 +1,14 @@
+﻿// CHIntegration.cs created by Iron Wolf for PMCompactHediffIIntegration on 04/03/2020 3:39 PM
+// last updated 04/03/2020  3:39 PM
+
+using Verse;
+
+namespace PMCompactHediffIIntegration
+{
+    public class CHIntegration : Mod
+    {
+        public CHIntegration(ModContentPack content) : base(content)
+        {
+        }
+    }
+}
---
+++ Source/Pawnmorphs/PMCompactHediffIIntegration/CHIntegrationInit.cs
@@ -0,0 +1,16 @@
+﻿// CHIntegrationInit.cs created by Iron Wolf for PMCompactHediffIIntegration on 04/03/2020 3:39 PM
+// last updated 04/03/2020  3:39 PM
+
+using Verse;
+
+namespace PMCompactHediffIIntegration
+{
+    [StaticConstructorOnStartup]
+    public static class CHIntegrationInit
+    {
+        static CHIntegrationInit()
+        {
+            Log.Message("$$$$$ CHIntegration Init Loaded!");
+        }
+    }
+}
