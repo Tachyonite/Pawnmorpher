64d1a4f5543aeccb945cb99db8e7016432c06235
FxWolfxe <ndmuench@comcast.net>
2020-04-19 19:11:08 -0400

    fixed former humans opening doors when they're not supposed to

---
+++ Source/Pawnmorphs/Esoteria/HPatches/DoorPatches.cs
@@ -0,0 +1,25 @@
+﻿// DoorPatches.cs created by Iron Wolf for Pawnmorph on 04/19/2020 6:59 PM
+// last updated 04/19/2020  6:59 PM
+
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Pawnmorph.HPatches
+{
+    [HarmonyPatch(typeof(Building_Door))]
+    static class DoorPatches
+    {
+
+        [HarmonyPatch(nameof(Building_Door.PawnCanOpen)), HarmonyPostfix]
+        static bool FixFormerHumanDoorPatch(bool result, Pawn p, Building_Door __instance)
+        {
+            if (result)
+            {
+                if (p.Faction != __instance.Faction && p.IsFormerHuman()) return false; 
+            }
+
+            return result; 
+        }
+    }
+}
