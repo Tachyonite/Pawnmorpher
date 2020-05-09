b09c72cfdd04fc4000c8e4cd543c4d6aa7e3e088
FxWolfxe <ndmuench@comcast.net>
2020-05-09 09:24:15 -0400

    animalistic prisoners can now use animal beds in prison cells
    fixes #112

--- Source/Pawnmorphs/Esoteria/HPatches/BedPatches.cs
+++ Source/Pawnmorphs/Esoteria/HPatches/BedPatches.cs
@@ -8,18 +8,50 @@ using Verse;
 
 namespace Pawnmorph.HPatches
 {
-    [HarmonyPatch(typeof(RestUtility), nameof(RestUtility.CanUseBedEver))]
-    static class BedPatches
+    [HarmonyPatch(typeof(RestUtility))]
+    internal static class BedPatches
     {
-        static void Postfix(ref bool __result, [NotNull] Pawn p, ThingDef bedDef)
+        [HarmonyPatch(nameof(RestUtility.CanUseBedEver))]
+        [HarmonyPostfix]
+        private static void CanUsebedEverPostfix(ref bool __result, [NotNull] Pawn p, ThingDef bedDef)
         {
             if (!__result && p.GetIntelligence() == Intelligence.Humanlike)
             {
-                __result = bedDef?.building?.bed_humanlike == true; 
-            }else if (__result && p.GetIntelligence() < Intelligence.ToolUser)
+                __result = bedDef?.building?.bed_humanlike == true;
+            }
+            else if (__result && p.GetIntelligence() < Intelligence.ToolUser)
             {
                 BuildingProperties building = bedDef?.building;
-                __result = building != null &&  p.BodySize <= (double) building.bed_maxBodySize;
+                __result = building != null && p.BodySize <= (double) building.bed_maxBodySize;
+            }
+        }
+
+        [HarmonyPatch(nameof(RestUtility.IsValidBedFor))]
+        [HarmonyPostfix]
+        private static void IsValidBedForFix(ref bool __result, Thing bedThing, Pawn sleeper, Pawn traveler,
+                                             bool sleeperWillBePrisoner,
+                                             bool checkSocialProperness, bool allowMedBedEvenIfSetToNoCare,
+                                             bool ignoreOtherReservations)
+        {
+            if (!__result && sleeperWillBePrisoner && sleeper.GetIntelligence() == Intelligence.Animal)
+            {
+                if (!bedThing.Position.IsInPrisonCell(bedThing.Map)) return;
+
+                BuildingProperties building = bedThing.def.building;
+                __result = building?.bed_humanlike == false && sleeper.BodySize <= building.bed_maxBodySize;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Building_Bed))]
+    static class BedBuildingPatches
+    {
+        [HarmonyPatch(nameof(Building_Bed.ForPrisoners), MethodType.Getter) ,HarmonyPostfix]
+        static void FixForPrisoner(ref bool __result, Building_Bed __instance)
+        {
+            if (!__result)
+            {
+                __result = __instance.def.building?.bed_humanlike == false && __instance.Position.IsInPrisonCell(__instance.Map); 
             }
         }
     }
