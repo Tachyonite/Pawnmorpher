60e9fdf6937b121c3cef8d69b915f7177ef4bb70
FxWolfxe <ndmuench@comcast.net>
2020-06-30 16:31:35 -0400

    fixed possible null reference exception

--- Source/Pawnmorphs/Esoteria/TfSys/SimpleMechaniteMutagen.cs
+++ Source/Pawnmorphs/Esoteria/TfSys/SimpleMechaniteMutagen.cs
@@ -201,13 +201,13 @@ namespace Pawnmorph.TfSys
 
 
             //apply any other post tf effects 
-            ApplyPostTfEffects(original, spawnedAnimal, request);
+            ApplyPostTfEffects(original, animalToSpawn, request);
 
             TransformerUtility
                .CleanUpHumanPawnPostTf(original, request.cause); //now clean up the original pawn (remove apparel, drop'em, ect) 
 
             //notify the faction that their member has been transformed 
-            oFaction?.Notify_MemberTransformed(original, spawnedAnimal, oMap == null, oMap);
+            oFaction?.Notify_MemberTransformed(original, animalToSpawn, oMap == null, oMap);
 
             if (!request.noLetter && reactionStatus == FormerHumanReactionStatus.Colonist || reactionStatus == FormerHumanReactionStatus.Prisoner) //only send the letter for colonists and prisoners 
                 SendLetter(request, original, spawnedAnimal);
