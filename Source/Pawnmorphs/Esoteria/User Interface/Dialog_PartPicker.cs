ddf062cad8ee86e516d0160deeea40913785998d
Brennen Cramer <48333768+BrennenCramer@users.noreply.github.com>
2020-07-23 17:48:54 -0700

    Finally got the Part Picker preview working.

--- Source/Pawnmorphs/Esoteria/User Interface/Dialog_PartPicker.cs
+++ Source/Pawnmorphs/Esoteria/User Interface/Dialog_PartPicker.cs
@@ -1,11 +1,12 @@
-﻿using Pawnmorph.Hediffs;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System;
 using UnityEngine;
-using RimWorld;
 using Verse;
 using Verse.Sound;
+using RimWorld;
+using AlienRace;
+using Pawnmorph.Hediffs;
 
 namespace Pawnmorph.User_Interface
 {
@@ -50,6 +51,12 @@ namespace Pawnmorph.User_Interface
         private bool doSymmetry = true;
         private Rot4 previewRot = Rot4.South;
 
+        // Preview related variables
+        public bool forceRecachePreview = false;
+        private GameObject gameObject;
+        private Camera camera;
+        private RenderTexture previewImage;
+
         public override Vector2 InitialSize
         {
             get
@@ -78,11 +85,11 @@ namespace Pawnmorph.User_Interface
             if (!confirmed)
             {
                 ResetPawnHealth();
-                SoundDefOf.Click.PlayOneShotOnCamera(null);
+                SoundDefOf.Click.PlayOneShotOnCamera();
             }
             else
             {
-                SoundDefOf.Checkbox_TurnedOn.PlayOneShotOnCamera(null);
+                SoundDefOf.Checkbox_TurnedOn.PlayOneShotOnCamera();
             }
             base.Close(doCloseSound);
         }
@@ -152,6 +159,7 @@ namespace Pawnmorph.User_Interface
                                             pawn.health.RemoveHediff(hediff);
                                         }
                                     }
+                                    forceRecachePreview = true;
                                 };
                                 options.Add(new FloatMenuOption(NO_MUTATIONS_LOC_STRING.Translate(), removeAction));
                                 foreach (MutationDef mutationDef in applicableMutations)
@@ -169,6 +177,7 @@ namespace Pawnmorph.User_Interface
                                         {
                                             MutationUtilities.AddMutation(pawn, mutationDef, bpr);
                                         }
+                                        forceRecachePreview = true;
                                     };
                                     options.Add(new FloatMenuOption(mutationDef.LabelCap, action));
                                 }
@@ -205,6 +214,7 @@ namespace Pawnmorph.User_Interface
                                 Action removeAction = delegate ()
                                 {
                                     if (mutationOnPartAndLayer != null) pawn.health.RemoveHediff(mutationOnPartAndLayer);
+                                    forceRecachePreview = true;
                                 };
                                 options.Add(new FloatMenuOption(NO_MUTATIONS_LOC_STRING.Translate(), removeAction));
                                 foreach (MutationDef mutationDef in applicableMutations)
@@ -213,6 +223,7 @@ namespace Pawnmorph.User_Interface
                                     {
                                         if (mutationOnPartAndLayer != null) pawn.health.RemoveHediff(mutationOnPartAndLayer);
                                         MutationUtilities.AddMutation(pawn, mutationDef, part);
+                                        forceRecachePreview = true;
                                     };
                                     options.Add(new FloatMenuOption(mutationDef.LabelCap, action));
                                 }
@@ -235,30 +246,35 @@ namespace Pawnmorph.User_Interface
             Widgets.EndScrollView();
 
             // Step 5 - Draw the preview area then rotation and clothes buttons then symmetry toggle.
-            // Draw Preview - Damn this code is hard
+            if (forceRecachePreview || previewImage == null)
+            {
+                setPawnPreview();
+            }
+            GUI.DrawTexture(previewRect, previewImage);
+            col2 += previewRect.height;
             float rotCWHorPos = previewRect.x + previewRect.width / 2 - TOGGLE_CLOTHES_BUTTON_SIZE.x / 2 - ROTATE_CW_BUTTON_SIZE.x - SPACER_SIZE;
             float toggleClothingHorPos = previewRect.x + previewRect.width / 2 - TOGGLE_CLOTHES_BUTTON_SIZE.x / 2;
             float rotCCWHorPos = previewRect.x + previewRect.width / 2 + TOGGLE_CLOTHES_BUTTON_SIZE.x / 2 + SPACER_SIZE;
             col2 += SPACER_SIZE;
-            if (Widgets.ButtonImageFitted(new Rect(rotCWHorPos, col2, ROTATE_CW_BUTTON_SIZE.x, ROTATE_CW_BUTTON_SIZE.y), ButtonTexturesPM.rotCW))
+            if (Widgets.ButtonImageFitted(new Rect(rotCWHorPos, col2, ROTATE_CW_BUTTON_SIZE.x, ROTATE_CW_BUTTON_SIZE.y), ButtonTexturesPM.rotCW, Color.white, Color.blue))
             {
                 previewRot.Rotate(RotationDirection.Clockwise);
+                SoundDefOf.Tick_Tiny.PlayOneShotOnCamera();
+                forceRecachePreview = true;
             }
-            if (Widgets.ButtonImageFitted(new Rect(toggleClothingHorPos, col2, TOGGLE_CLOTHES_BUTTON_SIZE.x, TOGGLE_CLOTHES_BUTTON_SIZE.y), ButtonTexturesPM.toggleClothes))
+            if (Widgets.ButtonImageFitted(new Rect(toggleClothingHorPos, col2, TOGGLE_CLOTHES_BUTTON_SIZE.x, TOGGLE_CLOTHES_BUTTON_SIZE.y), ButtonTexturesPM.toggleClothes, Color.white, Color.blue))
             {
                 toggleClothesEnabled = !toggleClothesEnabled;
+                (toggleClothesEnabled ? SoundDefOf.Checkbox_TurnedOn : SoundDefOf.Checkbox_TurnedOff).PlayOneShotOnCamera();
+                forceRecachePreview = true;
             }
-            if (Widgets.ButtonImageFitted(new Rect(rotCCWHorPos, col2, ROTATE_CCW_BUTTON_SIZE.x, ROTATE_CCW_BUTTON_SIZE.y), ButtonTexturesPM.rotCCW))
+            if (Widgets.ButtonImageFitted(new Rect(rotCCWHorPos, col2, ROTATE_CCW_BUTTON_SIZE.x, ROTATE_CCW_BUTTON_SIZE.y), ButtonTexturesPM.rotCCW, Color.white, Color.blue))
             {
                 previewRot.Rotate(RotationDirection.Counterclockwise);
+                SoundDefOf.Tick_Tiny.PlayOneShotOnCamera();
+                forceRecachePreview = true;
             }
             col2 += Math.Max(TOGGLE_CLOTHES_BUTTON_SIZE.y, Math.Max(ROTATE_CW_BUTTON_SIZE.y, ROTATE_CCW_BUTTON_SIZE.y));
-            // Temporary debug code
-            Widgets.Label(new Rect(previewRect.x, col2, previewRect.width, 24f), $"Rotation: {previewRot.ToStringHuman()}");
-            col2 += 24f;
-            Widgets.Label(new Rect(previewRect.x, col2, previewRect.width, 24f), $"Clothing: {(toggleClothesEnabled ? "Enabled" : "Disabled")}");
-            col2 += 24f;
-            // End temporary debug code
             string toggleText = DO_SYMMETRY_LOC_STRING.Translate();
             float toggleTextHeight = Text.CalcHeight(toggleText, PREVIEW_SIZE.x);
             Widgets.CheckboxLabeled(new Rect(inRect.x + SPACER_SIZE + drawableWidth, col2, PREVIEW_SIZE.x, toggleTextHeight), DO_SYMMETRY_LOC_STRING.Translate(), ref doSymmetry);
@@ -279,6 +295,7 @@ namespace Pawnmorph.User_Interface
             {
                 SoundDefOf.Checkbox_TurnedOff.PlayOneShotOnCamera(null);
                 ResetPawnHealth();
+                forceRecachePreview = true;
             }
             if (Widgets.ButtonText(new Rect(cancelHorPos, buttonVertPos, CANCEL_BUTTON_SIZE.x, CANCEL_BUTTON_SIZE.y), CANCEL_BUTTON_LOC_STRING.Translate()))
             {
@@ -289,6 +306,141 @@ namespace Pawnmorph.User_Interface
         public void ResetPawnHealth()
         {
             pawn.health.hediffSet.hediffs = new List<Hediff>(cachedHediffList);
+        }
+
+        public void setPawnPreview()
+        {
+            if (pawn != null)
+            {
+                forceRecachePreview = false;
+                pawn.Drawer.renderer.graphics.ResolveAllGraphics();
+                PortraitsCache.SetDirty(pawn);
+                RenderPawn();
+                InitCamera();
+                camera.gameObject.SetActive(true);
+                camera.transform.position = new Vector3(0f, pawn.Position.y + 1f, 0f);
+                camera.forceIntoRenderTexture = true;
+                camera.targetTexture = previewImage;
+                camera.Render();
+                camera.targetTexture = null;
+                camera.forceIntoRenderTexture = false;
+                camera.gameObject.SetActive(false);
+            }
+            else
+            {
+                Log.Error("Pawn was null when attempting to recache part picker preview.");
+            }
+        }
+
+        // Taken from RenderingTool.RenderPawnInternal in CharacterEditor
+        private void RenderPawn()
+        {
+            PawnGraphicSet graphics = pawn.Drawer.renderer.graphics;
+            Quaternion quaternion = Quaternion.AngleAxis(0f, Vector3.up);
+
+            Mesh bodyMesh = pawn.RaceProps.Humanlike ? MeshPool.humanlikeBodySet.MeshAt(previewRot) : graphics.nakedGraphic.MeshAt(previewRot);
+            Vector3 bodyOffset = new Vector3 (0f, pawn.Position.y + 0.007575758f, 0f);
+            foreach (Material mat in graphics.MatsBodyBaseAt(previewRot))
+            {
+                Material damagedMat = graphics.flasher.GetDamagedMat(mat);
+                GenDraw.DrawMeshNowOrLater(bodyMesh, bodyOffset, quaternion, damagedMat, false);
+                bodyOffset.y += 0.00390625f;
+                if (!toggleClothesEnabled)
+                {
+                    break;
+                }
+            }
+            Vector3 vector3 = new Vector3(0f, pawn.Position.y + (previewRot == Rot4.North ? 0.026515152f : 0.022727273f), 0f);
+            Vector3 vector4 = new Vector3(0f, pawn.Position.y + (previewRot == Rot4.North ? 0.022727273f : 0.026515152f), 0f);
+            if (graphics.headGraphic != null)
+            {
+                Mesh mesh2 = MeshPool.humanlikeHeadSet.MeshAt(previewRot);
+                Vector3 headOffset = quaternion * pawn.Drawer.renderer.BaseHeadOffsetAt(previewRot);
+                Material material = graphics.HeadMatAt(previewRot);
+                GenDraw.DrawMeshNowOrLater(mesh2, vector4 + headOffset, quaternion, material, false);
+
+                Mesh hairMesh = graphics.HairMeshSet.MeshAt(previewRot);
+                Vector3 hairOffset = new Vector3(headOffset.x, pawn.Position.y + 0.030303031f, headOffset.z);
+                bool isWearingHat = false;
+                if (toggleClothesEnabled)
+                {
+                    foreach (ApparelGraphicRecord apparel in graphics.apparelGraphics)
+                    {
+                        if (apparel.sourceApparel.def.apparel.LastLayer == ApparelLayerDefOf.Overhead)
+                        {
+                            Material hatMat = graphics.flasher.GetDamagedMat(apparel.graphic.MatAt(previewRot));
+                            if (apparel.sourceApparel.def.apparel.hatRenderedFrontOfFace)
+                            {
+                                isWearingHat = true;
+                                hairOffset.y += 0.03f;
+                                GenDraw.DrawMeshNowOrLater(hairMesh, hairOffset, quaternion, hatMat, false);
+                            }
+                            else
+                            {
+                                Vector3 hatOffset = new Vector3(headOffset.x, pawn.Position.y + (previewRot == Rot4.North ? 0.003787879f : 0.03409091f), headOffset.z);
+                                GenDraw.DrawMeshNowOrLater(hairMesh, hatOffset, quaternion, hatMat, false);
+                            }
+                        }
+                    }
+                }
+                if (!isWearingHat)
+                {
+                    Material hairMat = graphics.HairMatAt(previewRot);
+                    GenDraw.DrawMeshNowOrLater(hairMesh, hairOffset, quaternion, hairMat, false);
+                }
+            }
+            if (toggleClothesEnabled)
+            {
+                foreach (ApparelGraphicRecord graphicsSet in graphics.apparelGraphics)
+                {
+                    if (graphicsSet.sourceApparel.def.apparel.LastLayer == ApparelLayerDefOf.Shell)
+                    {
+                        Material clothingMat = graphics.flasher.GetDamagedMat(graphicsSet.graphic.MatAt(previewRot));
+                        GenDraw.DrawMeshNowOrLater(bodyMesh, vector3, quaternion, clothingMat, false);
+                    }
+                }
+            }
+            HarmonyPatches.DrawAddons(false, vector3, pawn, quaternion, previewRot, false);
+            if (toggleClothesEnabled)
+            {
+                if (pawn.apparel != null)
+                {
+                    foreach (Apparel apparel in pawn.apparel.WornApparel)
+                    {
+                        apparel.DrawWornExtras();
+                    }
+                }
+            }
+        }
+
+
+        internal void InitCamera()
+        {
+            if (gameObject == null)
+            {
+                gameObject = new GameObject("PreviewCamera", new Type[]
+                {
+                    typeof(Camera)
+                });
+                gameObject.SetActive(false);
+            }
+            if (camera == null)
+            {
+                camera = gameObject.GetComponent<Camera>();
+                camera.transform.position = new Vector3(0f, 1f, 0f);
+                camera.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+                camera.orthographic = true;
+                camera.orthographicSize = 1f;
+                camera.clearFlags = CameraClearFlags.Color;
+                camera.backgroundColor = new Color(0f, 0f, 0f, 0f);
+                camera.renderingPath = RenderingPath.Forward;
+                camera.nearClipPlane = Current.Camera.nearClipPlane;
+                camera.farClipPlane = Current.Camera.farClipPlane;
+                camera.targetTexture = null;
+                camera.forceIntoRenderTexture = false;
+                previewImage = new RenderTexture(200, 280, 24);
+                camera.targetTexture = previewImage;
+            }
         }
     }
 }
