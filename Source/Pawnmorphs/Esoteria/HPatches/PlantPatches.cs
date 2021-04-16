656bb967290ad3fc4ccc2f79e87299f9ec23b48a
FxWolfxe <ndmuench@comcast.net>
2021-04-16 17:34:50 -0400

    failed chaobulb harvests give buildup

--- Source/Pawnmorphs/Esoteria/HPatches/PlantPatches.cs
+++ Source/Pawnmorphs/Esoteria/HPatches/PlantPatches.cs
@@ -1,9 +1,16 @@
 ﻿// PlantPatches.cs modified by Iron Wolf for Pawnmorph on 12/31/2019 3:25 PM
 // last updated 12/31/2019  3:25 PM
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
+using HugsLib.Utils;
 using JetBrains.Annotations;
 using Pawnmorph.DefExtensions;
+using Pawnmorph.Plants;
 using RimWorld;
 using Verse;
 using Verse.AI;
@@ -55,6 +62,61 @@ namespace Pawnmorph.HPatches
                 var plant = WorkGiver_Grower.CalculateWantedPlantDef(c, pawn.Map);
                 if (plant == null) return;
                 __result = plant.IsValidFor(pawn);
+            }
+        }
+
+        [HarmonyPatch]
+        public static class PlantHarvestTPatch
+        {
+            public static MethodInfo match = typeof(Plant).GetMethod("YieldNow");
+            public static MethodInfo replaceWith = typeof(PlantHarvestTPatch).GetMethod("YieldNowPatch");
+            public static MethodInfo TargetMethod()
+            {
+                Type mainType = typeof(JobDriver_PlantWork);
+                //Log.Message("TargetMethod: Main Type Found");
+
+
+                BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
+                try
+                {
+                    Type iteratorType = mainType.GetNestedTypes(bindingFlags).First(t => t.FullName.Contains("c__DisplayClass"));
+                    //Log.Message("TargetMethod: Iterator Type Resolved");
+                    //Type anonStoreyType = iteratorType.GetNestedTypes(BindingFlags.NonPublic | BindingFlags.Instance).First(t => t.FullName.Contains("b__1"));
+                    //Log.Message("TargetMethod: AnonStorey Type Resolved");
+                    return iteratorType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public).First(m => m.ReturnType == typeof(void));
+                }
+                catch (InvalidOperationException iO)
+                {
+                    var names = mainType.GetNestedTypes(bindingFlags).Select(t => t.FullName).Join(","); 
+
+                    throw new InvalidOperationException($"unable to find type with \"c__DisplayClass\" among \"{names}\""); 
+                }
+
+            }
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+            {
+                foreach (CodeInstruction i in instr)
+                {
+                    if (i.operand == match)
+                    {
+                        //Log.Message("Instruction insertion complete!");
+                        yield return new CodeInstruction(OpCodes.Ldloc_0);
+                        yield return new CodeInstruction(OpCodes.Call, replaceWith);
+                    }
+                    else
+                    {
+                        yield return i;
+                    }
+                }
+            }
+            public static int YieldNowPatch(Plant p, Pawn actor)
+            {
+                if (p is SpecialHarvestFailPlant sPlant)
+                {
+                    return sPlant.GetYieldNow(actor); 
+                }
+
+                return p.YieldNow(); // Whatever you want to do here
             }
         }
     }
---
+++ Source/Pawnmorphs/Esoteria/Plants/SpecialHarvestFailPlant.cs
@@ -0,0 +1,50 @@
+﻿// SpecialHarvestFailPlant.cs created by Iron Wolf for Pawnmorph on 04/16/2021 1:03 PM
+// last updated 04/16/2021  1:03 PM
+
+using RimWorld;
+using Verse;
+
+namespace Pawnmorph.Plants
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="RimWorld.Plant" />
+    public abstract class SpecialHarvestFailPlant : Plant
+    {
+        /// <summary>
+        /// Gets the yield now.
+        /// </summary>
+        /// <param name="harvester">The harvester.</param>
+        /// <returns></returns>
+        public abstract int GetYieldNow(Pawn harvester); 
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="Pawnmorph.Plants.SpecialHarvestFailPlant" />
+    public class Chaobulb : SpecialHarvestFailPlant
+    {
+        /// <summary>
+        /// Gets the yield now.
+        /// </summary>
+        /// <param name="harvester">The harvester.</param>
+        /// <returns></returns>
+        public override int GetYieldNow(Pawn harvester)
+        {
+            var failed = harvester.RaceProps.Humanlike 
+                      && !Blighted
+                             && Rand.Value > harvester.GetStatValue(StatDefOf.PlantHarvestYield) ;
+
+            if(failed)
+            {
+                MutagenicBuildupUtilities.AdjustMutagenicBuildup(def, harvester, 0.1f);
+                return 0; 
+            }
+
+            return YieldNow(); 
+        }
+    }
+}
