fac6791cbc3ded5e75e7200190d2a4ea737fb800
FxWolfxe <ndmuench@comcast.net>
2021-07-11 20:50:12 -0400

    quick change and formatting

--- Source/Pawnmorphs/Esoteria/Hediffs/Comp_MutationSeverityAdjust.cs
+++ Source/Pawnmorphs/Esoteria/Hediffs/Comp_MutationSeverityAdjust.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using JetBrains.Annotations;
 using Pawnmorph.Utilities;
 using RimWorld;
-using RimWorld.Planet;
 using UnityEngine;
 using Verse;
 
@@ -19,20 +18,87 @@ namespace Pawnmorph.Hediffs
     /// <seealso cref="Pawnmorph.Utilities.HediffCompBase{T}" />
     public class Comp_MutationSeverityAdjust : HediffComp_SeverityPerDay
     {
+        private const float EPSILON = 0.001f;
         private bool _halted;
 
         private float? _statAdjust;
 
-        float StatAdjust
+        private int _curStageIndex = -1;
+
+
+        private float? _mutationAdaptabilityCache;
+
+        private bool _checkForReversionHediff;
+
+
+        /// <summary>
+        ///     Gets the natural severity limit.
+        /// </summary>
+        /// this value is the value the attached hediff should reach if the pawn has had the mutation for an 'infinite' amount of time
+        /// <value>
+        ///     The natural severity limit.
+        /// </value>
+        public float NaturalSeverityLimit => Props.GetNaturalSeverityLimitFor(Pawn);
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether this <see cref="Comp_MutationSeverityAdjust" /> is halted.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if halted; otherwise, <c>false</c>.
+        /// </value>
+        public bool Halted
+        {
+            get => _halted;
+            set
+            {
+                if (_halted != value)
+                {
+                    foreach (Hediff_AddedMutation allSimilarMutation in GetAllSimilarMutations())
+                    {
+                        Comp_MutationSeverityAdjust sevComp = allSimilarMutation.SeverityAdjust;
+                        if (sevComp != null)
+                        {
+                            sevComp._halted =
+                                value; //use the private variable so we don't trigger an infinite loop of comps setting each other
+                            allSimilarMutation.Severity = parent.Severity; //make sure they have the same severity to 
+                        }
+                    }
+
+                    _halted = value;
+                }
+            }
+        }
+
+        
+        
+        /// <summary>
+        ///     Gets a value indicating whether the parent hediff should be removed.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if [comp should remove]; otherwise, <c>false</c>.
+        /// </value>
+        public override bool CompShouldRemove => SeverityChangePerDay() < 0 && parent.CurStageIndex == 0;
+
+        /// <summary>
+        ///     Gets the change per day.
+        /// </summary>
+        /// <value>
+        ///     The change per day.
+        /// </value>
+        public float ChangePerDay => SeverityChangePerDay();
+
+        
+        //TODO make some sort of unified caching system for stat lookups, easy way to cause lots of lag 
+        private float StatAdjust
         {
             get
             {
                 if (_statAdjust == null)
-                {
-                    _statAdjust = Pawn.GetStatValue(PMStatDefOf.MutagenSensitivity); //make mutagen sensitivity influence how fast the adjustment works 
-                }
+                    _statAdjust =
+                        Pawn.GetStatValue(PMStatDefOf
+                                             .MutagenSensitivity); //make mutagen sensitivity influence how fast the adjustment works 
 
-                return _statAdjust.Value; 
+                return _statAdjust.Value;
             }
         }
 
@@ -57,15 +123,18 @@ namespace Pawnmorph.Hediffs
             }
         }
 
+        private int TickRate => Pawn.SpawnedOrAnyParentSpawned ? 25 : 70;
 
-        /// <summary>
-        ///     Gets the natural severity limit.
-        /// </summary>
-        /// this value is the value the attached hediff should reach if the pawn has had the mutation for an 'infinite' amount of time
-        /// <value>
-        ///     The natural severity limit.
-        /// </value>
-        public float NaturalSeverityLimit => Props.GetNaturalSeverityLimitFor(Pawn);
+        private float MutationAdaptability //should we make a central stat caching class? 
+        {
+            get
+            {
+                if (_mutationAdaptabilityCache == null)
+                    _mutationAdaptabilityCache = Pawn.GetStatValue(PMStatDefOf.MutationAdaptability);
+
+                return _mutationAdaptabilityCache.Value;
+            }
+        }
 
         /// <summary>
         ///     called to save/load data for this comp.
@@ -78,63 +147,15 @@ namespace Pawnmorph.Hediffs
         }
 
         /// <summary>
-        /// Restarts this instance.
+        ///     called when the parent is merged with a new hediff of the same type
         /// </summary>
-        public void Restart()
+        /// <param name="other">The other.</param>
+        public override void CompPostMerged(Hediff other)
         {
-            Halted = false;
-            _mutationAdaptabilityCache = null; 
-            _statAdjust = null; 
+            base.CompPostMerged(other);
+            Halted = false; //restart adjusting if halted 
+            _statAdjust = null;
         }
-
-        /// <summary>
-        ///     Gets or sets a value indicating whether this <see cref="Comp_MutationSeverityAdjust" /> is halted.
-        /// </summary>
-        /// <value>
-        ///     <c>true</c> if halted; otherwise, <c>false</c>.
-        /// </value>
-        public bool Halted
-        {
-            get => _halted;
-            set
-            {
-                if (_halted != value)
-                {
-                    foreach (Hediff_AddedMutation allSimilarMutation in GetAllSimilarMutations())
-                    {
-                        Comp_MutationSeverityAdjust sevComp = allSimilarMutation.SeverityAdjust;
-                        if (sevComp != null)
-                        {
-                            sevComp._halted =
-                                value; //use the private variable so we don't trigger an infinite loop of comps setting each other
-                            allSimilarMutation.Severity = parent.Severity; //make sure they have the same severity to 
-
-                        }
-                    }
-
-                    _halted = value;
-                }
-            }
-        }
-
-        [NotNull]
-        private IEnumerable<Hediff_AddedMutation> GetAllSimilarMutations()
-        {
-            List<Hediff> hediffSet = Pawn.health?.hediffSet?.hediffs;
-            foreach (Hediff_AddedMutation hediff in hediffSet.MakeSafe().OfType<Hediff_AddedMutation>())
-                if (hediff != parent && hediff.def == parent.def)
-                    yield return hediff;
-        }
-
-        private int _curStageIndex = -1;
-
-        /// <summary>
-        /// Gets a value indicating whether the parent hediff should be removed.
-        /// </summary>
-        /// <value>
-        ///   <c>true</c> if [comp should remove]; otherwise, <c>false</c>.
-        /// </value>
-        public override bool CompShouldRemove => SeverityChangePerDay() < 0 && parent.CurStageIndex == 0; 
 
         /// <summary>
         ///     called every tick
@@ -151,7 +172,7 @@ namespace Pawnmorph.Hediffs
                 CheckIfHalted();
             }
 
-            int tickRate = Pawn.SpawnedOrAnyParentSpawned ? 25 : 70; //prioritize updating spawned pawns over world pawns 
+            int tickRate = TickRate; //prioritize updating spawned pawns over world pawns 
 
             if (Pawn.IsHashIntervalTick(tickRate))
             {
@@ -161,17 +182,69 @@ namespace Pawnmorph.Hediffs
         }
 
         /// <summary>
-        ///     called when the parent is merged with a new hediff of the same type
+        ///     Recalcs the adjust speed.
         /// </summary>
-        /// <param name="other">The other.</param>
-        public override void CompPostMerged(Hediff other)
+        public void RecalcAdjustSpeed()
         {
-            base.CompPostMerged(other);
-            Halted = false; //restart adjusting if halted 
+            _checkForReversionHediff = true;
+        }
+
+        /// <summary>
+        ///     Restarts this instance.
+        /// </summary>
+        public void Restart()
+        {
+            Halted = false;
+            _mutationAdaptabilityCache = null;
             _statAdjust = null;
         }
 
-        private const float EPSILON = 0.001f;
+        /// <summary>
+        ///     get the change in severity per day
+        /// </summary>
+        /// <returns></returns>
+        public override float SeverityChangePerDay()
+        {
+            if (_checkForReversionHediff || Pawn.IsHashIntervalTick(TickRate))
+            {
+                _checkForReversionHediff = false;
+                bool hasReversionHediff = Pawn.health?.hediffSet?.HasHediff(MorphTransformationDefOf.PM_Reverting) == true;
+                if (hasReversionHediff) return -1;
+            }
+
+            if (Halted) return 0;
+            float statValue = MutationAdaptability;
+            float maxSeverity = Mathf.Max(statValue + 1, 1);
+            float minSeverity = Mathf.Min(statValue, 0); //have the stat influence how high or low the severity can be 
+            float sMult = Props.statEffectMult * (statValue + 1);
+            float sevPerDay = base.SeverityChangePerDay() * sMult;
+            //make sure the severity can only stay between the max and min 
+            if (parent.Severity > maxSeverity) sevPerDay = Mathf.Min(0, sevPerDay);
+            if (parent.Severity < minSeverity) sevPerDay = Mathf.Max(0, sevPerDay);
+
+
+            return sevPerDay * Mathf.Max(StatAdjust, 0); //take the mutagen sensitivity stat into account 
+        }
+
+        private void CheckIfHalted()
+        {
+            if (parent.CurStage is MutationStage mStage)
+            {
+                float stopChance = mStage.stopChance * GetChanceMult() * Pawn.GetStatValue(PMStatDefOf.MutationHaltChance);
+
+                if (stopChance < 0.01f) return;
+                if (Rand.Value < stopChance) Halted = true;
+            }
+        }
+
+        [NotNull]
+        private IEnumerable<Hediff_AddedMutation> GetAllSimilarMutations()
+        {
+            List<Hediff> hediffSet = Pawn.health?.hediffSet?.hediffs;
+            foreach (Hediff_AddedMutation hediff in hediffSet.MakeSafe().OfType<Hediff_AddedMutation>())
+                if (hediff != parent && hediff.def == parent.def)
+                    yield return hediff;
+        }
 
         private float GetChanceMult()
         {
@@ -186,94 +259,6 @@ namespace Pawnmorph.Hediffs
             sVal *= -2 / r; //shift the range again to [0,1], where a default stat value is 0, and either min or max is 0 
             sVal = Mathf.Max(sVal, 0); //make sure it doesn't go below zero, can happen if the default is not the center point of min and max
             return sVal;
-        }
-
-        //disabled because it caused large amounts of lag, enable only if debugging 
-#if false
-        /// <summary>
-        /// gets the debug string 
-        /// </summary>
-        /// <returns></returns>
-        public override string CompDebugString()
-        {
-            StringBuilder builder = new StringBuilder();
-            builder.AppendLine(base.CompDebugString());
-            builder.AppendLine($"stop multiplier is {GetChanceMult()}");
-            return builder.ToString(); 
-        }
-
-#endif
-
-        private void CheckIfHalted()
-        {
-            if (parent.CurStage is MutationStage mStage)
-            {
-                float stopChance = mStage.stopChance * GetChanceMult() * Pawn.GetStatValue(PMStatDefOf.MutationHaltChance);
-
-                if (stopChance < 0.01f) return; 
-                if (Rand.Value < stopChance) Halted = true;
-            }
-        }
-
-
-        private float? _mutationAdaptabilityCache;
-
-        float MutationAdaptability //should we make a central stat caching class? 
-        {
-            get
-            {
-                if (_mutationAdaptabilityCache == null)
-                {
-                    _mutationAdaptabilityCache = Pawn.GetStatValue(PMStatDefOf.MutationAdaptability); 
-                }
-
-                return _mutationAdaptabilityCache.Value; 
-            }
-        }
-
-        /// <summary>
-        ///     get the change in severity per day
-        /// </summary>
-        /// <returns></returns>
-        public override float SeverityChangePerDay()
-        {
-            if (_checkForReversionHediff || Pawn.IsHashIntervalTick(25))
-            {
-                var hasReversionHediff = Pawn.health?.hediffSet?.HasHediff(MorphTransformationDefOf.PM_Reverting) == true;
-                if (hasReversionHediff) return -1; 
-
-            }
-
-            if (Halted) return 0;
-            float statValue = MutationAdaptability; 
-            float maxSeverity = Mathf.Max(statValue + 1, 1);
-            float minSeverity = Mathf.Min(statValue, 0); //have the stat influence how high or low the severity can be 
-            float sMult = Props.statEffectMult * (statValue + 1);
-            var sevPerDay = base.SeverityChangePerDay() * sMult;
-            //make sure the severity can only stay between the max and min 
-            if (parent.Severity > maxSeverity) sevPerDay = Mathf.Min(0, sevPerDay);
-            if (parent.Severity < minSeverity) sevPerDay = Mathf.Max(0, sevPerDay);
-
-
-            return sevPerDay * Mathf.Max(StatAdjust, 0); //take the mutagen sensitivity stat into account 
-        }
-
-        /// <summary>
-        /// Gets the change per day.
-        /// </summary>
-        /// <value>
-        /// The change per day.
-        /// </value>
-        public float ChangePerDay => SeverityChangePerDay();
-
-        private bool _checkForReversionHediff;
-
-        /// <summary>
-        /// Recalcs the adjust speed.
-        /// </summary>
-        public void RecalcAdjustSpeed()
-        {
-            _checkForReversionHediff = true; 
         }
     }
 
