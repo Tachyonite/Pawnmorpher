a49709b9694b8ea5fd0819fbc88094827378ebee
Loïc <86140855+lelodu87@users.noreply.github.com>
2021-07-14 22:03:08 +0200

    New skew normal distribution
    If you're allergic to maths, don't touch that.

--- Source/Pawnmorphs/Esoteria/Utilities/RandUtilities.cs
+++ Source/Pawnmorphs/Esoteria/Utilities/RandUtilities.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using Verse;
+using System;
 
 namespace Pawnmorph.Utilities
 {
@@ -15,12 +16,35 @@ namespace Pawnmorph.Utilities
         /// <param name="sigma">The Standard deviation.</param>
         public static float generateNormalRandom(float mu = 0, float sigma = 1)
         {
+            if (sigma <= 0)
+                throw new ArgumentException("Standard deviation cannot be null");
+
             float rand1 = Rand.Range(0.0f, 1.0f);
             float rand2 = Rand.Range(0.0f, 1.0f);
 
             float n = Mathf.Sqrt(-2.0f * Mathf.Log(rand1)) * Mathf.Cos(2.0f * Mathf.PI * rand2);
 
             return mu + sigma * n;
+        }
+
+        /// <summary>
+        ///     Generate a random number according to a Skew Normal distribution (non symmetric).
+        /// </summary>
+        /// <param name="loc"> The location (not actually the mean).</param>
+        /// <param name="scale">The scale (not actually the standard deviation).</param>
+        /// <param name="shape">The shape, determining the skewness. Negative if you want only a few values after the mode (value that appears the most), positive before.</param>
+        public static float generateSkewNormalRandom(float loc, float scale, float shape)
+        {
+            if (scale <= 0)
+                throw new ArgumentException("Scale cannot be null");
+
+            float corr = shape / Mathf.Sqrt(1 + Mathf.Pow(shape, 2));
+            float u0 = generateNormalRandom(0,1);
+            float v = generateNormalRandom(0,1);
+            float u1 = corr * u0 + Mathf.Sqrt(1 - Mathf.Pow(corr, 2)) * v;
+
+            float y = u0 > 0 ? u1 : -u1;
+            return loc + scale * y;
         }
 
         /// <summary>
