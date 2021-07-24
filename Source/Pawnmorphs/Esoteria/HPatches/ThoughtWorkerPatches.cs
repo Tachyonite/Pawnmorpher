804898e828309f52eda3fbffe392667290fde4a0
FxWolfxe <ndmuench@comcast.net>
2021-07-24 11:53:16 -0400

    patched nudity thought workers

---
+++ Source/Pawnmorphs/Esoteria/HPatches/ThoughtWorkerPatches.cs
@@ -0,0 +1,98 @@
+﻿// ThoughtWorkerPatches.cs created by Iron Wolf for Pawnmorph on 07/24/2021 10:56 AM
+// last updated 07/24/2021  10:56 AM
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using JetBrains.Annotations;
+using RimWorld;
+using Verse;
+
+namespace Pawnmorph.HPatches
+{
+    /// <summary>
+    /// patches thought workers 
+    /// </summary>
+    public static class ThoughtWorkerPatches
+    {
+        /// <summary>
+        /// patches thought worker.
+        /// </summary>
+        /// <param name="harInstance">The har instance.</param>
+        public static void DoPatches([NotNull] Harmony harInstance)
+        {
+            try
+            {
+                BindingFlags stBindingFlags = BindingFlags.NonPublic | BindingFlags.Static;
+                var normalPatchMethod = typeof(ThoughtWorkerPatches).GetMethod("DisableFormerHumanThoughtNormal", stBindingFlags);
+                var socialPatchMethod = typeof(ThoughtWorkerPatches).GetMethod("DisableFormerHumanThoughtSocial", stBindingFlags);
+
+
+                foreach (MethodInfo method in AllNormalMethods)
+                {
+                    harInstance.Patch(method, postfix: new HarmonyMethod(normalPatchMethod)); 
+                }
+
+                foreach (MethodInfo allSocialMethod in AllSocialMethods)
+                {
+                    harInstance.Patch(allSocialMethod, postfix: new HarmonyMethod(socialPatchMethod)); 
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error($"unable to perform thought worker patching!\n{e}");
+            }
+        }
+
+
+        static IEnumerable<Type> AllPreceptNudityThoughts
+        {
+            get
+            {
+                var maleNudity = DefDatabase<IssueDef>.GetNamed("Nudity_Male");
+                var femaleNudity = DefDatabase<IssueDef>.GetNamed("Nudity_Female");
+
+                var precepts = DefDatabase<PreceptDef>.AllDefs.Where(p => p.issue == maleNudity || p.issue == femaleNudity);
+                var comps = precepts.SelectMany(p => p.comps.OfType<PreceptComp_SituationalThought>());
+                var types = comps.Select(t => t.thought.workerClass);
+                return types; 
+            }
+        }
+
+
+        static IEnumerable<MethodInfo> AllNormalMethods
+        {
+            get
+            {
+                return AllPreceptNudityThoughts.Where(tp => !tp.Name.Contains("Social"))
+                                               .Select(t => t.GetMethod("ShouldHaveThought", 
+                                                                        BindingFlags.NonPublic | BindingFlags.Instance));
+            }
+        }
+
+
+        static IEnumerable<MethodInfo> AllSocialMethods
+        {
+            get
+            {
+                return AllPreceptNudityThoughts.Where(tp => tp.Name.Contains("Social"))
+                                               .Select(t => t.GetMethod("ShouldHaveThought",
+                                                                        BindingFlags.NonPublic | BindingFlags.Instance));
+            }
+        }
+
+
+        static void DisableFormerHumanThoughtNormal(Pawn p, ref bool __result)
+        {
+            __result = __result && !p.IsFormerHuman(); 
+        }
+
+        static void DisableFormerHumanThoughtSocial(Pawn p, Pawn otherPawn, ref bool __result)
+        {
+            __result = __result && !otherPawn.IsFormerHuman();
+        }
+
+    }
+}
