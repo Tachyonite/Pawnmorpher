b3a3f25f17c9fa1a8b0020b22cfebf4d0992f579
Loïc <86140855+lelodu87@users.noreply.github.com>
2021-07-25 19:43:32 +0200

    Precept reactions
    WIP. Only for now a PreceptComp used to distinguish colonists/visitors/prisonners/others.

---
+++ Source/Pawnmorphs/Esoteria/PreceptComps/PMPreceptComp_KnowsMemoryThought.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace Pawnmorph.PreceptComps
+{
+    class PMPreceptComp_KnowsMemoryThought : PreceptComp_Thought
+    {
+        public HistoryEventDef eventDef;
+
+        public override IEnumerable<TraitRequirement> TraitsAffecting => ThoughtUtility.GetNullifyingTraits(thought);
+
+        public override void Notify_MemberWitnessedAction(HistoryEvent ev, Precept precept, Pawn member)
+        {
+            if (ev.def != eventDef)
+                return;
+
+            Pawn victimPawn;
+            bool flag = ev.args.TryGetArg(HistoryEventArgsNames.Doer, out victimPawn);
+            if (!flag) //In case something goes wrong. But it should not.
+                return;
+
+            if (victimPawn == member)
+                return;
+            
+            int stage = 3;
+            if (!member.IsColonist)
+                stage = 3;
+            else if (member.IsColonist && victimPawn.IsColonist)
+                stage = 0;
+            else if (victimPawn.IsPrisonerOfColony)
+                stage = 1;
+            else if (victimPawn.guest?.HostFaction == Faction.OfPlayer)
+                stage = 2;
+
+            Thought_Memory thought_Memory = ThoughtMaker.MakeThought(thought, precept);
+            thought_Memory.SetForcedStage(Math.Min(stage, thought.stages.Count - 1));
+
+        }
+    }
+}
