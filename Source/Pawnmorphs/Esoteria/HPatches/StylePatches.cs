87acab36f6f30ef7043f71915b379e8803b1adcb
FxWolfxe <ndmuench@comcast.net>
2021-07-28 17:34:12 -0400

    disabled style changes for former humans

---
+++ Source/Pawnmorphs/Esoteria/HPatches/StylePatches.cs
@@ -0,0 +1,21 @@
+﻿// StylePatches.cs created by Iron Wolf for Pawnmorph on 07/28/2021 5:31 PM
+// last updated 07/28/2021  5:31 PM
+
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Pawnmorph.HPatches
+{
+    static class StylePatches
+    {
+        [HarmonyPatch(typeof(Pawn_StyleTracker), nameof(Pawn_StyleTracker.CanDesireLookChange), MethodType.Getter)]
+        static class DisableStyleTrackerForFH
+        {
+            static void Postfix(Pawn ___pawn, ref bool __result)
+            {
+                if (__result) __result = !(___pawn?.IsFormerHuman() ?? true);
+            }
+        }
+    }
+}
