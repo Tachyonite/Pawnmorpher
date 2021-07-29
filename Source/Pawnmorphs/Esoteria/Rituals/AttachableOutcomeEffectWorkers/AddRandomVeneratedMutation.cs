0f54d487c37bdb8cfd3acf7d5e4425d2aeb24869
FxWolfxe <ndmuench@comcast.net>
2021-07-29 19:06:48 -0400

    created mutation ritual outcome

---
+++ Source/Pawnmorphs/Esoteria/Rituals/AttachableOutcomeEffectWorkers/AddRandomVeneratedMutation.cs
@@ -0,0 +1,177 @@
+﻿// AddRandomVeneratedMutation.cs created by Iron Wolf for Pawnmorph on 07/29/2021 5:31 PM
+// last updated 07/29/2021  5:31 PM
+
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Pawnmorph.Hediffs;
+using Pawnmorph.Utilities;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Pawnmorph.Rituals.AttachableOutcomeEffectWorkers
+{
+    /// <summary>
+    ///     ritual outcome effect worker that adds a random number of mutations of a venerated animal on a target
+    /// </summary>
+    /// <seealso cref="RimWorld.RitualAttachableOutcomeEffectWorker" />
+    public class AddRandomVeneratedMutation : RitualAttachableOutcomeEffectWorker
+    {
+        /// <summary>
+        ///     translation id for the translated outcome text
+        /// </summary>
+        protected const string MUTATION_TRANSLATION = "PM_PawnMutatated_Ritual";
+
+
+        /// <summary>
+        ///     the tag that refers to the number of pawns mutated in the ritual
+        /// </summary>
+        protected const string COUNT_TAG = "Count";
+
+        /// <summary>
+        ///     the tag for members of this ritual
+        /// </summary>
+        protected const string MEMBER_TAG = "Member";
+
+        /// <summary>
+        ///     Applies the effect on the given ritual .
+        /// </summary>
+        /// <param name="totalPresence">The total presence.</param>
+        /// <param name="jobRitual">The job ritual.</param>
+        /// <param name="outcome">The outcome.</param>
+        /// <param name="extraOutcomeDesc">The extra outcome desc.</param>
+        /// <param name="letterLookTargets">The letter look targets.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override void Apply(Dictionary<Pawn, int> totalPresence, LordJob_Ritual jobRitual, OutcomeChance outcome,
+                                   out string extraOutcomeDesc,
+                                   ref LookTargets letterLookTargets)
+        {
+            InitRitual(jobRitual, outcome);
+            IEnumerable<Pawn> pawns = GetTargets(jobRitual, outcome);
+
+            var count = 0;
+            var scratchList = new List<MutationDef>();
+            foreach (Pawn pawn in pawns)
+            {
+                Pawn_HealthTracker health = pawn.health;
+                if (health == null) continue;
+                int mutationCount = GetMutationCount(pawn, outcome);
+                if (mutationCount == 0) continue;
+                scratchList.Clear();
+                scratchList.AddRange(GetMutationsToAdd(pawn, jobRitual, outcome, out _));
+
+                int mAdded = 0, tries = 0;
+                const int maxTries = 100;
+                while (mAdded < mutationCount && tries < maxTries && scratchList.Count > 0)
+                {
+                    tries++;
+
+                    MutationDef mut = scratchList.RandomElement();
+                    scratchList.Remove(mut);
+                    if (health.hediffSet.GetFirstHediffOfDef(mut) != null) continue;
+
+                    MutationUtilities.AddMutation(pawn, mut);
+                    mAdded++;
+                }
+
+                count++;
+            }
+
+            if (count > 0)
+            {
+                Ideo ideo = jobRitual.Ritual.ideo;
+                string mName = count > 1 ? ideo.MemberNamePlural : ideo.memberName;
+                extraOutcomeDesc = def.letterInfoText.Formatted(count.Named(COUNT_TAG), mName.Named(MEMBER_TAG));
+            }
+            else
+            {
+                extraOutcomeDesc = "";
+            }
+        }
+
+        /// <summary>
+        ///     Determines whether this instance with the specified ritual can be applied
+        /// </summary>
+        /// <param name="ritual">The ritual.</param>
+        /// <param name="map">The map.</param>
+        /// <returns></returns>
+        public override AcceptanceReport CanApplyNow(Precept_Ritual ritual, Map map)
+        {
+            return ritual.ideo.VeneratedAnimals.MakeSafe().Any(a => a.TryGetBestMorphOfAnimal() != null);
+        }
+
+        /// <summary>
+        ///     Gets the mutation count.
+        /// </summary>
+        /// <param name="pawn">The pawn.</param>
+        /// <param name="outcomeChance">The outcome chance.</param>
+        /// <returns></returns>
+        protected virtual int GetMutationCount([NotNull] Pawn pawn, [NotNull] OutcomeChance outcomeChance)
+        {
+            return outcomeChance.positivityIndex;
+        }
+
+
+        /// <summary>
+        ///     Gets the mutations to add onto the given target
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <param name="jobRitual">The job ritual.</param>
+        /// <param name="outcome">The outcome.</param>
+        /// <returns></returns>
+        [NotNull]
+        protected virtual IEnumerable<MutationDef> GetMutationsToAdd([NotNull] Pawn target, [NotNull] LordJob_Ritual jobRitual,
+                                                                     [NotNull] OutcomeChance outcome, out ThingDef chosenAnimal)
+        {
+            Ideo ideo = jobRitual.Ritual.ideo;
+            if (ideo == null)
+            {
+                Log.Error($"unable to find ideo while getting mutations for outcome of {def.defName}!");
+                chosenAnimal = null;
+                return Enumerable.Empty<MutationDef>();
+            }
+
+            if (!ideo.VeneratedAnimals.MakeSafe()
+                     .Select(a => (a, a.TryGetBestMorphOfAnimal()))
+                     .Where(tup => tup.Item2 != null)
+                     .TryRandomElement(out (ThingDef a, MorphDef) rTup))
+            {
+                Log.Error($"{def.defName} is unable to get valid venerated animal from {ideo.name} to get mutations from!");
+                chosenAnimal = null;
+                return Enumerable.Empty<MutationDef>();
+            }
+
+            chosenAnimal = rTup.a;
+            return rTup.Item2.AllAssociatedMutations;
+        }
+
+        /// <summary>
+        ///     Gets the targets to add mutations onto
+        /// </summary>
+        /// <param name="jobRitual">The job ritual.</param>
+        /// <returns></returns>
+        [NotNull]
+        protected virtual IEnumerable<Pawn> GetTargets([NotNull] LordJob_Ritual jobRitual, [NotNull] OutcomeChance outcome)
+        {
+            RitualRole role = jobRitual.GetRole(RoleTags.TARGET_TAG);
+            IEnumerable<Pawn> enumer;
+            if (role == null) enumer = jobRitual.assignments.Participants.Where(p => MutagenDefOf.defaultMutagen.CanInfect(p));
+            else
+                enumer = (jobRitual.assignments?.AssignedPawns(role)).MakeSafe();
+            int take = Mathf.Max(0, outcome.positivityIndex);
+            if (take == 0) return Enumerable.Empty<Pawn>();
+            return enumer.Take(take);
+        }
+
+
+        /// <summary>
+        ///     Initializes the ritual effects. called when starting to process the effects of this ritual
+        /// </summary>
+        /// <param name="ritual">The ritual.</param>
+        /// <param name="outcome">The outcome.</param>
+        protected virtual void InitRitual([NotNull] LordJob_Ritual ritual, [NotNull] OutcomeChance outcome)
+        {
+        }
+    }
+}
---
+++ Source/Pawnmorphs/Esoteria/Rituals/RoleTags.cs
@@ -0,0 +1,16 @@
+﻿// RoleTags.cs created by Iron Wolf for Pawnmorph on 07/29/2021 5:36 PM
+// last updated 07/29/2021  5:36 PM
+
+namespace Pawnmorph.Rituals
+{
+    /// <summary>
+    /// static class containing commonly used ritual role tags
+    /// </summary>
+    public static class RoleTags
+    {
+        /// <summary>
+        /// tag used to define a 'target' role, used to assign outcomes of an effect onto a specific role 
+        /// </summary>
+        public static string TARGET_TAG = "target"; 
+    }
+}
