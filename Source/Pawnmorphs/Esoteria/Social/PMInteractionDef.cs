fec6db62ba48f41213a6f3afdf6ed0e8cb7fb5e6
Pausbrak <rslego@gmail.com>
2021-08-08 20:47:09 -0400

    fixed many issues with the XML

--- Source/Pawnmorphs/Esoteria/Social/PMInteractionDef.cs
+++ Source/Pawnmorphs/Esoteria/Social/PMInteractionDef.cs
@@ -21,9 +21,9 @@ namespace Pawnmorph.Social
         public class Weights
         {
             /// <summary>At least one of these mutations is required for this interaction to trigger.</summary>
-            public HashSet<HediffDef> requiredMutationsAny = new HashSet<HediffDef>();
+            public List<HediffDef> requiredMutationsAny = new List<HediffDef>();
             /// <summary>All of these mutations are required before this interaction triggers.</summary>
-            public HashSet<HediffDef> requiredMutationsAll = new HashSet<HediffDef>();
+            public List<HediffDef> requiredMutationsAll = new List<HediffDef>();
             /// <summary>The mutation weights</summary>
             public Dictionary<HediffDef, float> mutationWeights = new Dictionary<HediffDef, float>();
             /// <summary>The morph weights</summary>
@@ -43,14 +43,16 @@ namespace Pawnmorph.Social
                 var hasMutation = false;
 
                 if (requiredMutationsAny.Count > 0 && !requiredMutationsAny.Any(hediffs.Contains)) return 0;
-                if (!requiredMutationsAll.IsSubsetOf(hediffs)) return 0;
+                if (requiredMutationsAll.Count > 0 && !requiredMutationsAll.All(hediffs.Contains)) return 0;
 
                 foreach (HediffDef hediffDef in hediffs)
+                {
                     if (mutationWeights.TryGetValue(hediffDef, out float v))
                     {
                         hasMutation = true;
                         total += v;
                     }
+                }
 
                 bool isMorph = RaceGenerator.TryGetMorphOfRace(pawn.def, out MorphDef morph);
 
