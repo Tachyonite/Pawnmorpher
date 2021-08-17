739fc56d076da7e9a3e62462b5a50c8b8ab279d7
Pausbrak <rslego@gmail.com>
2021-08-17 02:21:19 -0400

    Add a damageworker for mutarabies bites

---
+++ Source/Pawnmorphs/Esoteria/Damage/DamageWorker_MutarabidBite.cs
@@ -0,0 +1,21 @@
+﻿using Verse;
+
+namespace Pawnmorph.Damage
+{
+    /// <summary>
+    /// Damage worker for a mutarabies-infected bite attack
+    /// </summary>
+    /// <seealso cref="Pawnmorph.Damage.Worker_MutagenicInjury" />
+    /// <seealso cref="Verse.DamageWorker_Bite" />
+    public class DamageWorker_MutarabidBite : Worker_MutagenicInjury
+    {
+        /// <summary>Chooses the hit part.</summary>
+        /// <param name="dinfo">The dInfo.</param>
+        /// <param name="pawn">The pawn.</param>
+        /// <returns></returns>
+        protected override BodyPartRecord ChooseHitPart(DamageInfo dinfo, Pawn pawn)
+        {
+            return pawn.health.hediffSet.GetRandomNotMissingPart(dinfo.Def, dinfo.Height, BodyPartDepth.Outside, null);
+        }
+    }
+}
