580c14390562758cfe080ccc2bd3b396d2000068
Pausbrak <rslego@gmail.com>
2021-08-17 19:07:35 -0400

    Add HediffStage classes for mutation and transformation

---
+++ Source/Pawnmorphs/Esoteria/Hediffs/HediffStage_MutagenicBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Verse;
+
+namespace Pawnmorph.Hediffs
+{
+    /// <summary>
+    /// Abstract base class for all hediff stages that involve mutation or
+    /// transformation, for use with Hediff_MutagenicBase.
+    /// </summary>
+    /// <seealso cref="Verse.HediffStage" />
+    /// <seealso cref="Pawnmorph.Hediffs.IDescriptiveStage" />
+    /// <seealso cref="Pawnmorph.Hediffs.Hediff_MutagenicBase" />
+    public abstract class HediffStage_MutagenicBase : HediffStage, IDescriptiveStage
+    {
+        //TODO
+        public string DescriptionOverride => throw new NotImplementedException();
+        public string LabelOverride => throw new NotImplementedException();
+    }
+}
---
+++ Source/Pawnmorphs/Esoteria/Hediffs/HediffStage_TransformationBase.cs
@@ -0,0 +1,19 @@
+﻿using Pawnmorph.Hediffs.Composable;
+
+namespace Pawnmorph.Hediffs
+{
+    /// <summary>
+    /// Abstract base class for all hediff stages that full transformations.
+    /// Any components defined in this class will override the equivalent component
+    /// in the parent hediff
+    /// </summary>
+    /// <seealso cref="Verse.HediffStage" />
+    /// <seealso cref="Pawnmorph.Hediffs.IDescriptiveStage" />
+    /// <seealso cref="Pawnmorph.Hediffs.Hediff_MutagenicBase" />
+    public abstract class HediffStage_TransformationBase : HediffStage_MutagenicBase
+    {
+        private MutSpreadOrder spreadOrder;
+        private MutRate mutationRate;
+        private MutTypes mutationTypes;
+    }
+}
