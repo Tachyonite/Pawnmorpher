776bc634f21578603950caf472836b1eca3aa33b
Pausbrak <rslego@gmail.com>
2021-08-17 02:10:51 -0400

    Add a mutarabies-specific Hediff for handling transfering the animal type

---
+++ Source/Pawnmorphs/Esoteria/Hediffs/Hediff_MutarabiesBite.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using Verse;
+
+namespace Pawnmorph.Hediffs
+{
+    /// <summary>
+    /// The TF hediff for Mutarabies bites
+    /// </summary>
+    /// <seealso cref="Verse.Hediff" />
+    public class Hediff_MutarabiesBite : Hediff_DynamicTf, IOnApplyViaAttack
+    {
+        /// <summary>
+        /// Tries the merge with the other hediff
+        /// </summary>
+        /// <param name="other">The other.</param>
+        /// <returns></returns>
+        public override bool TryMergeWith(Hediff other)
+        {
+            if (other is MutagenicBuildup buildup)
+            {
+                Severity += other.Severity;
+                foreach (HediffComp hediffComp in comps)
+                {
+                    hediffComp.CompPostMerged(other);
+                }
+
+                ResetMutationOrder();
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// A callback that's called when this hediff is added via an injury
+        /// </summary>
+        /// <param name="dinfo">the damage info of the attack that added the hediff</param>
+        public void OnApplyViaAttack(DamageInfo dinfo)
+        {
+            ThingDef attackerDef = dinfo.Instigator?.def;
+            if (attackerDef == null)
+            {
+                Log.Warning($"{pawn.Name} was given Mutarabies but the attacker was null, defaulting to random morphdef");
+                return;
+            }
+
+            // TODO - rejigger this so we can apply the animal type even if there's not an associated morph type
+            MorphDef infectionType = DefDatabase<MorphDef>.AllDefs.FirstOrDefault(m => m.race == attackerDef);
+            if (infectionType != null)
+            {
+                MorphDef = infectionType;
+            }
+        }
+    }
+}
--- Source/Pawnmorphs/Esoteria/Hediffs/IOnApplyViaAttack.cs
+++ Source/Pawnmorphs/Esoteria/Hediffs/IOnApplyViaAttack.cs
@@ -7,12 +7,12 @@ namespace Pawnmorph.Hediffs
     /// Interaface that adds a callback to the hediff that will be called when it
     /// is applied via a mutagenic injury
     /// </summary>
-    public interface IOnApplyViaInjury
+    public interface IOnApplyViaAttack
     {
         /// <summary>
         /// A callback that's called when this hediff is added via an injury
         /// </summary>
         /// <param name="dinfo">the damage info of the attack that added the hediff</param>
-        void OnApplyViaInjury(DamageInfo dinfo);
+        void OnApplyViaAttack(DamageInfo dinfo);
     }
 }
