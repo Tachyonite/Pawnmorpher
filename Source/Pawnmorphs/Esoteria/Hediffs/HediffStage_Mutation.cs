086767a287a319755227c6391dae438cce00ba30
Pausbrak <rslego@gmail.com>
2021-08-17 22:50:21 -0400

    Add a HediffDef_Mutagenic for mutagenic hediffs

---
+++ Source/Pawnmorphs/Esoteria/Hediffs/HediffStage_Mutation.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using Pawnmorph.Hediffs.Composable;
+
+namespace Pawnmorph.Hediffs
+{
+    /// <summary>
+    /// Class for handling all hediffs that cause mutations and transformation
+    /// Any components defined in this class will override the equivalent component
+    /// in the parent hediff
+    /// </summary>
+    /// <seealso cref="Pawnmorph.IDescriptiveHediff" />
+    /// <seealso cref="Verse.Hediff" />
+    public class HediffStage_Mutation : HediffStage_MutagenicBase
+    {
+        [UsedImplicitly] private MutSpreadOrder spreadOrder;
+        [UsedImplicitly] private MutRate mutationRate;
+        [UsedImplicitly] private MutTypes mutationTypes;
+
+        public MutSpreadOrder SpreadOrder => spreadOrder;
+        public MutRate MutationRate => mutationRate;
+        public MutTypes MutationTypes => mutationTypes;
+    }
+}
---
+++ Source/Pawnmorphs/Esoteria/Hediffs/HediffStage_Transformation.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using Pawnmorph.Hediffs.Composable;
+
+namespace Pawnmorph.Hediffs
+{
+    /// <summary>
+    /// Class for all hediff stages that full transformations.
+    /// Any components defined in this class will override the equivalent component
+    /// in the parent hediff
+    /// </summary>
+    /// <seealso cref="Verse.HediffStage" />
+    /// <seealso cref="Pawnmorph.Hediffs.IDescriptiveStage" />
+    /// <seealso cref="Pawnmorph.Hediffs.Hediff_MutagenicBase" />
+    public class HediffStage_Transformation : HediffStage_MutagenicBase
+    {
+        [UsedImplicitly] private TFTypes transformationTypes;
+        [UsedImplicitly] private TFGenderSettings transformationGenderSettings;
+        [UsedImplicitly] private TFMiscSettings transformationSettings;
+
+        public TFTypes TFTypes => transformationTypes;
+        public TFGenderSettings TFGenderSettings => transformationGenderSettings;
+        public TFMiscSettings TFMiscSettings => transformationSettings;
+    }
+}
