4b90ef715bb8142a91fe9700394fac52da3dc71e
FxWolfxe <ndmuench@comcast.net>
2021-08-24 19:23:02 -0400

    added def extension and renamed part group
    created def extension to give body part groups more properties and renamed the body part group used in nagas

---
+++ Source/Pawnmorphs/Esoteria/DefExtensions/BodyPartGroupExtension.cs
@@ -0,0 +1,22 @@
+﻿// BodyPartGroupExtension.cs created by Iron Wolf for Pawnmorph on 08/24/2021 6:35 PM
+// last updated 08/24/2021  6:35 PM
+
+using Verse;
+
+namespace Pawnmorph.DefExtensions
+{
+    /// <summary>
+    /// extended properties for body part group defs 
+    /// </summary>
+    /// <seealso cref="Verse.DefModExtension" />
+    public class BodyPartGroupExtension : DefModExtension
+    {
+        /// <summary>
+        /// if this group should count as the groin for nudity checks 
+        /// </summary>
+        public bool countsAsGroin;
+
+
+
+    }
+}
