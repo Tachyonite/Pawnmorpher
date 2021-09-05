fcb4b756f7a800612527166720fbdce2354a033a
Pausbrak <rslego@gmail.com>
2021-09-05 00:38:56 -0400

    Thoughts for helping and rejecting relatives

--- Source/Pawnmorphs/Esoteria/Letters/ChoiceLetter_FormerHumanJoins.cs
+++ Source/Pawnmorphs/Esoteria/Letters/ChoiceLetter_FormerHumanJoins.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using RimWorld;
 using RimWorld.QuestGen;
 using Verse;
@@ -86,6 +88,25 @@ namespace Pawnmorph.Letters
 
             Find.LetterStack.ReceiveLetter(letter, null);
         }
+        /// <summary>
+        /// Whether this letter can be dismissed with a right click
+        /// </summary>
+        /// <value><c>true</c> if can dismiss with right click; otherwise, <c>false</c>.</value>
+        public override bool CanDismissWithRightClick => false;
+
+        /// <summary>
+        /// Whether this letter is still valid to be shown on the stack
+        /// </summary>
+        /// <value><c>true</c> if can show in letter stack; otherwise, <c>false</c>.</value>
+        public override bool CanShowInLetterStack
+        {
+            get
+            {
+                if (!base.CanShowInLetterStack)
+                    return false;
+                return FormerHumanStillJoinable();
+            }
+        }
 
         /// <summary>
         /// The possible choices of the letter
@@ -101,41 +122,134 @@ namespace Pawnmorph.Letters
                 }
                 else
                 {
-                    // TODO translate
-                    DiaOption diaOption = new DiaOption("RansomDemand_Accept".Translate()) 
-                    {
-                        action = delegate
-                        {
-                            formerHuman.SetFaction(Faction.OfPlayer);
-                            CameraJumper.TryJump(formerHuman.Position, formerHuman.Map);
-                            Find.LetterStack.RemoveLetter(this);
-                        },
-                        resolveTree = true
-                    };
-                    yield return diaOption;
-                    yield return Option_Reject;
-                    yield return Option_Postpone;
-
                     if (lookTargets.IsValid())
                         yield return Option_JumpToLocationAndPostpone;
+
+                    yield return Option_Accept;
+                    yield return Option_RejectWithCharityConfirmation;
+                    yield return Option_Postpone;
                 }
             }
         }
 
-        /// <summary>
-        /// Whether this letter is still valid to be shown on the stack
-        /// </summary>
-        /// <value><c>true</c> if can show in letter stack; otherwise, <c>false</c>.</value>
-        public override bool CanShowInLetterStack
+        private DiaOption Option_Accept
         {
             get
             {
-                if (!base.CanShowInLetterStack)
-                    return false;
-                return !formerHuman.DestroyedOrNull()
-                    && !formerHuman.Dead
-                    && formerHuman.Faction != Faction.OfPlayer;
+                DiaOption diaOption = new DiaOption("AcceptButton".Translate())
+                {
+                    action = delegate
+                    {
+                        formerHuman.SetFaction(Faction.OfPlayer);
+                        CameraJumper.TryJump(formerHuman.Position, formerHuman.Map);
+                        Find.LetterStack.RemoveLetter(this);
+
+                        if (ModsConfig.IdeologyActive)
+                            HistoryEventDefOf.CharityFulfilled_WandererJoins.SendEvent();
+                    },
+                    resolveTree = true
+                };
+
+                if (!FormerHumanStillJoinable())
+                    diaOption.Disable(null);
+                return diaOption;
             }
+        }
+
+        private DiaOption Option_RejectWithCharityConfirmation => new DiaOption("RejectLetter".Translate())
+        {
+            action = delegate {
+                void Reject()
+                {
+                    Find.LetterStack.RemoveLetter(this);
+                }
+
+                if (!ModsConfig.IdeologyActive)
+                {
+                    Reject();
+                }
+                else
+                {
+                    // Confirm if any pawn has a charity ideology
+                    IEnumerable<Pawn> source = IdeoUtility.AllColonistsWithCharityPrecept();
+                    if (source.Any())
+                    {
+                        string dialogText = "";
+                        foreach (IGrouping<Ideo, Pawn> item in from c in source
+                                                               group c by c.Ideo)
+                        {
+                            dialogText += "\n  - " + "BelieversIn".Translate(item.Key.name.Colorize(item.Key.TextColor), item.Select((Pawn f) => f.NameShortColored.Resolve()).ToCommaList());
+                        }
+                        var confirm = Dialog_MessageBox.CreateConfirmation("ConfirmationCharityJoiner".Translate(dialogText), Reject);
+                        Find.WindowStack.Add(confirm);
+                    }
+                    else
+                    {
+                        Reject();
+                    }
+                }
+            },
+            resolveTree = true
+        };
+
+        /// <summary>
+        /// Called after the letter is removed
+        /// </summary>
+        public override void Removed()
+        {
+            base.Removed();
+
+            // Make sure we always send the charity signal and add thoughts, regardless of why the letter was removed
+            // If you ignore their request for help and they get eaten by a predator, that still counts as rejecting them :D
+            if (formerHuman.Faction == Faction.OfPlayer)
+            {
+                // Gain a thought for helping family
+                ThoughtDef thought;
+                switch (GetCloseness())
+                {
+                    case RelationCloseness.VeryClose:
+                        thought = PMThoughtDefOf.PMFormerHumanAccepted_VeryClose;
+                        break;
+                    case RelationCloseness.Close:
+                        thought = PMThoughtDefOf.PMFormerHumanAccepted_Close;
+                        break;
+                    case RelationCloseness.Moderate:
+                        thought = PMThoughtDefOf.PMFormerHumanAccepted_Moderate;
+                        break;
+                    default:
+                        thought = PMThoughtDefOf.PMFormerHumanAccepted_Distant;
+                        break;
+                }
+                relative?.needs?.mood?.thoughts?.memories?.TryGainMemory(thought, formerHuman);
+
+                if (ModsConfig.IdeologyActive)
+                    HistoryEventDefOf.CharityFulfilled_WandererJoins.SendEvent();
+            }
+            else
+            {
+                // Gain a thought for not helping family
+                ThoughtDef thought;
+                switch (GetCloseness())
+                {
+                    case RelationCloseness.VeryClose:
+                        thought = PMThoughtDefOf.PMFormerHumanRejected_VeryClose;
+                        break;
+                    case RelationCloseness.Close:
+                        thought = PMThoughtDefOf.PMFormerHumanRejected_Close;
+                        break;
+                    case RelationCloseness.Moderate:
+                        thought = PMThoughtDefOf.PMFormerHumanRejected_Moderate;
+                        break;
+                    default:
+                        thought = PMThoughtDefOf.PMFormerHumanRejected_Distant;
+                        break;
+                }
+                relative?.needs?.mood?.thoughts?.memories?.TryGainMemory(thought, formerHuman);
+
+                if (ModsConfig.IdeologyActive)
+                    HistoryEventDefOf.CharityRefused_WandererJoins.SendEvent();
+            }
+
         }
 
         /// <summary>
@@ -147,6 +261,54 @@ namespace Pawnmorph.Letters
             Scribe_References.Look(ref formerHuman, nameof(formerHuman));
             Scribe_References.Look(ref relative, nameof(relative));
             Scribe_Defs.Look(ref relation, nameof(relation));
+        }
+
+        /// <summary>
+        /// Whether the pawn is still eligable to join
+        /// </summary>
+        /// <returns><c>true</c>, if human still joinable was formered, <c>false</c> otherwise.</returns>
+        private bool FormerHumanStillJoinable()
+        {
+            return !formerHuman.DestroyedOrNull()
+                    && !formerHuman.Dead
+                    && formerHuman.Faction != Faction.OfPlayer;
+        }
+
+        /// <summary>
+        /// Gets the closeness of the relationship.
+        /// </summary>
+        /// <returns>The closeness.</returns>
+        private RelationCloseness GetCloseness()
+        {
+
+            if (relation == PawnRelationDefOf.Child
+                || relation == PawnRelationDefOf.Spouse
+                || relation == PawnRelationDefOf.Fiance)
+                return RelationCloseness.VeryClose;
+
+            if (relation == PawnRelationDefOf.Parent
+                || relation == PawnRelationDefOf.Sibling
+                || relation == PawnRelationDefOf.Lover)
+                return RelationCloseness.Close;
+
+            if (relation == PawnRelationDefOf.Grandparent
+                || relation == PawnRelationDefOf.Grandchild
+                || relation == PawnRelationDefOf.UncleOrAunt
+                || relation == PawnRelationDefOf.NephewOrNiece
+                || relation == PawnRelationDefOf.Cousin
+                || relation == PawnRelationDefOf.HalfSibling)
+                return RelationCloseness.Moderate;
+
+            return RelationCloseness.Distant;
+        }
+
+        // How close a relative is, used for thought severity
+        private enum RelationCloseness
+        {
+            Distant,
+            Moderate,
+            Close,
+            VeryClose
         }
     }
 }
