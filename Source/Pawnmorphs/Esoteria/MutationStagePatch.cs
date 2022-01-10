03e7e3afc8d1fd36bb2f8505ba8b776a922808f6
Zeracronius <mathias-m-a@hotmail.com>
2022-01-10 23:27:24 +0100

    Polishing and switched to reflection to be completely dynamic.

--- Source/Pawnmorphs/Esoteria/MutationStagePatch.cs
+++ Source/Pawnmorphs/Esoteria/MutationStagePatch.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,17 +8,35 @@ using Verse;
 
 namespace Pawnmorph.Hediffs
 {
+    /// <summary>
+    /// Mutation stage patch that can be included in a <see cref="MutationDef" /> to allow modifying mutation stages in derived xml files with shared stages.
+    /// </summary>
     public class MutationStagePatch
     {
-        string identity;
+        /// <summary>
+        /// Identity of the stage to affect when using modify or remove function.
+        /// </summary>
+        [CanBeNull]
+        string identity = null;
+
+        /// <summary>
+        /// The patch behavior. Can be either "add", "modify" or "remove".
+        /// </summary>
+        [NotNull]
         string function = "modify";
+
+        /// <summary>
+        /// The mutation stage containing the values to use to either update an existing stage or append and entirely new stage.
+        /// </summary>
+        [CanBeNull]
         MutationStage values = null;
 
+        /// <summary>
+        /// Applies the specified stage patch.
+        /// </summary>
+        /// <param name="mutation">The mutation.</param>
         public void Apply(MutationDef mutation)
         {
-            if (values == null)
-                return;
-
             switch (function)
             {
                 case "add":
@@ -31,17 +50,27 @@ namespace Pawnmorph.Hediffs
                 case "remove":
                     Remove(mutation);
                     break;
+
+                default:
+                    Log.Warning($"Invalid mutation stage patch function: {function} in {mutation.ToString()}");
+                    break;
             }
 
         }
 
         private void Add(MutationDef mutation)
         {
+            if (values == null)
+                return;
+
             mutation.stages.Add(values);
         }
 
         private void Remove(MutationDef mutation)
         {
+            if (String.IsNullOrWhiteSpace(identity))
+                return;
+
             MutationStage stage = mutation.stages.FirstOrDefault(x => (x.overrideLabel ?? x.label) == identity) as MutationStage;
             if (stage != null)
                 mutation.stages.Remove(stage);
@@ -51,31 +80,37 @@ namespace Pawnmorph.Hediffs
 
         private void Modify(MutationDef mutation)
         {
+            if (String.IsNullOrWhiteSpace(identity))
+                return;
+
+            if (values == null)
+                return;
+
             MutationStage stage = mutation.stages.FirstOrDefault(x => (x.overrideLabel ?? x.label) == identity) as MutationStage;
             if (stage != null)
             {
-                if (values.graphics != null)
-                    stage.graphics = values.graphics;
+                // Get public instance fields that can be set.
+                System.Reflection.FieldInfo[] members = typeof(MutationStage).GetFields(System.Reflection.BindingFlags.Public | 
+                                                                                        System.Reflection.BindingFlags.Instance);
 
-                if (values.capMods != null)
-                    stage.capMods = values.capMods;
+                MutationStage defaultValues = new MutationStage();
+                foreach (System.Reflection.FieldInfo member in members)
+                {
+                    object newValue = member.GetValue(values);
 
-                if (values.description != null)
-                    stage.description = values.description;
+                    if (newValue != null)
+                    {
+                        // get default value
+                        object defaultValue = member.GetValue(defaultValues);
 
-                if (values.minSeverity > 0)
-                    stage.minSeverity = values.minSeverity;
+                        if (newValue.Equals(defaultValue))
+                            continue;
 
-
-                if (values.healthOffset > 0)
-                    stage.healthOffset = values.healthOffset;
-
-                if (values.labelOverride != null)
-                    stage.labelOverride = values.labelOverride;
-
-                if (values.stopChance > 0)
-                    stage.stopChance = values.stopChance;
+                        member.SetValue(stage, newValue);
+                    }
+                }
             }
         }
+
     }
 }
