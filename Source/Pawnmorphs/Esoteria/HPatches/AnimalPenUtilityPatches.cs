8b409dcf55c6b2f8072ac8a6eaad5113aff4ba5c
Zeracronius <mathias-m-a@hotmail.com>
2022-02-02 00:16:53 +0100

    Added transpiler patch for AnimalPenUtility.GetHitchingPostAnimalShouldBeTakenTo.
    closes #419

--- Source/Pawnmorphs/Esoteria/HPatches/AnimalPenUtilityPatches.cs
+++ Source/Pawnmorphs/Esoteria/HPatches/AnimalPenUtilityPatches.cs
@@ -2,6 +2,10 @@
 // last updated 07/06/2021  8:36 PM
 
 using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using Verse;
 
 namespace Pawnmorph.HPatches
@@ -19,6 +23,52 @@ namespace Pawnmorph.HPatches
             else
             {
                 __result = (pawn.IsWildMan()); 
+            }
+        }
+
+
+        public static bool IsRopeManagedAnimalDefPatched(Thing thing)
+        {
+            bool result = AnimalPenUtility.IsRopeManagedAnimalDef(thing.def);
+            if (thing is Pawn pawn)
+                NeedsToBeManagedByRopePatch(pawn, ref result);
+
+            return result;
+        }
+
+        [HarmonyPatch(typeof(AnimalPenUtility), nameof(AnimalPenUtility.GetHitchingPostAnimalShouldBeTakenTo))]
+        static class IsRopeManagedAnimalDefPatch
+        {
+            [HarmonyTranspiler]
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = instructions.ToList(); //convert the code instructions to a list so we can do 2 at a time 
+
+                FieldInfo defField = AccessTools.Field(typeof(Thing), nameof(Thing.def));
+                MethodInfo targetMethod = AccessTools.Method(typeof(AnimalPenUtility), nameof(AnimalPenUtility.IsRopeManagedAnimalDef));
+                MethodInfo subMethod = AccessTools.Method(typeof(AnimalPenUtilityPatches), nameof(IsRopeManagedAnimalDefPatched));
+
+                const int len = 5;
+                for (int i = 0; i < codes.Count - len; i++)
+                {
+                    // If call is made to taget method
+                    if (codes[i].opcode == OpCodes.Call && codes[i].operand as MethodInfo == targetMethod)
+                    {
+
+                        CodeInstruction animalDefFieldRef = codes[i - 1];
+                        if (animalDefFieldRef.opcode != OpCodes.Ldfld || (animalDefFieldRef.operand as FieldInfo) != defField)
+                            continue; // Skip
+
+                        // Replace method call
+                        codes[i].operand = subMethod;
+
+                        // Remove .def
+                        animalDefFieldRef.opcode = OpCodes.Nop;
+                        animalDefFieldRef.operand = null;
+                    }
+                }
+
+                return codes;
             }
         }
     }
