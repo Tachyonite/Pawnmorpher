ee5bb995576835cc03445df1af9d21957b54dbd6
Zeracronius <zeracronius@outlook.dk>
2022-02-09 18:19:20 +0100

    Disabled warning on setForbidden for unforbiddable things.
    closes #467

---
+++ Source/Pawnmorphs/Esoteria/HPatches/ForbidUtilityPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Pawnmorph.HPatches
+{
+    [HarmonyPatch(typeof(ForbidUtility))]
+    internal class ForbidUtilityPatch
+    {
+        [HarmonyPatch(nameof(ForbidUtility.SetForbidden)), HarmonyPrefix]
+        static void SetForbidden(Thing t, ref bool value, ref bool warnOnFail)
+        {
+            warnOnFail = false;
+        }
+
+
+    }
+}
