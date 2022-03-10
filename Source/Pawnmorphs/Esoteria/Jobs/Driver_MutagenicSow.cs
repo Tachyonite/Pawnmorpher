be8db6e766b87d6dc2385a7a68b2d9115e19f2b0
FxWolfxe <ndmuench@comcast.net>
2022-03-10 11:39:30 -0500

    added sow mutagenic plant jobs

--- Source/Pawnmorphs/Esoteria/Jobs/Driver_MutagenicSow.cs
+++ Source/Pawnmorphs/Esoteria/Jobs/Driver_MutagenicSow.cs
@@ -1,7 +1,8 @@
-﻿// Driver_SowMutagenicPlant.cs created by Iron Wolf for Pawnmorph on 03/09/2022 6:49 AM
-// last updated 03/09/2022  6:49 AM
+﻿// Driver_MutagenicSow.cs created by Iron Wolf for Pawnmorph on 03/10/2022 11:07 AM
+// last updated 03/10/2022  11:07 AM
 
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using RimWorld;
 using Verse;
 using Verse.AI;
@@ -11,23 +12,24 @@ namespace Pawnmorph.Jobs
     /// <summary>
     ///     job driver for sowing mutagenic plants
     /// </summary>
-    /// <seealso cref="RimWorld.JobDriver_PlantSow" />
-    public class Driver_SowMutagenicPlant : JobDriver
+    /// <seealso cref="Verse.AI.JobDriver" />
+    public class Driver_MutagenicSow : JobDriver
     {
-        private float _sowWorkDone;
+        private float sowWorkDone;
+
         private Plant Plant => (Plant) job.GetTarget(TargetIndex.A).Thing;
 
         /// <summary>
-        ///     Exposes the data for saving/loading.
+        ///     Exposes the data.
         /// </summary>
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Values.Look(ref _sowWorkDone, "sowWorkDone");
+            Scribe_Values.Look(ref sowWorkDone, "sowWorkDone");
         }
 
         /// <summary>
-        ///     Tries to make the pre toil reservations.
+        ///     Tries the make pre toil reservations.
         /// </summary>
         /// <param name="errorOnFailed">if set to <c>true</c> [error on failed].</param>
         /// <returns></returns>
@@ -35,7 +37,6 @@ namespace Pawnmorph.Jobs
         {
             return pawn.Reserve(job.targetA, job, 1, -1, null, errorOnFailed);
         }
-
 
         /// <summary>
         ///     Makes the new toils.
@@ -55,51 +56,59 @@ namespace Pawnmorph.Jobs
                 obj.Growth = 0f;
                 obj.sown = true;
             };
-            sowToil.tickAction = delegate { IncrementSowWork(sowToil.actor); };
+            sowToil.tickAction = delegate
+            {
+                Pawn actor = sowToil.actor;
+                if (actor != null)
+                    WorkPlant(actor);
+            };
             sowToil.defaultCompleteMode = ToilCompleteMode.Never;
             sowToil.FailOnDespawnedNullOrForbidden(TargetIndex.A);
             sowToil.FailOnCannotTouch(TargetIndex.A, PathEndMode.Touch);
             sowToil.WithEffect(EffecterDefOf.Sow, TargetIndex.A);
-            sowToil.WithProgressBar(TargetIndex.A, () => _sowWorkDone / Plant.def.plant.sowWork, true);
+            sowToil.WithProgressBar(TargetIndex.A, () => sowWorkDone / Plant.def.plant.sowWork, true);
             sowToil.PlaySustainerOrSound(() => SoundDefOf.Interact_Sow);
             sowToil.AddFinishAction(delegate
             {
                 if (TargetThingA != null)
                 {
                     var plant = (Plant) sowToil.actor.CurJob.GetTarget(TargetIndex.A).Thing;
-                    if (_sowWorkDone < plant.def.plant.sowWork && !TargetThingA.Destroyed) TargetThingA.Destroy();
+                    if (sowWorkDone < plant.def.plant.sowWork && !TargetThingA.Destroyed) TargetThingA.Destroy();
                 }
             });
             sowToil.activeSkill = () => SkillDefOf.Plants;
             yield return sowToil;
         }
 
-        private void IncrementSowWork(Pawn actor)
+        private void WorkPlant([NotNull] Pawn actor)
         {
             if (actor.skills != null) actor.skills.Learn(SkillDefOf.Plants, 0.085f);
-            float statValue = actor.GetStatValue(StatDefOf.PlantWorkSpeed);
+            float statValue = actor.GetStatValue(StatDefOf.PlantWorkSpeed); 
             Plant plant2 = Plant;
             if (plant2.LifeStage != 0) Log.Error(string.Concat(this, " getting sowing work while not in Sowing life stage."));
-            _sowWorkDone += statValue;
-            if (_sowWorkDone >= plant2.def.plant.sowWork) SowPlant(plant2, actor);
+            sowWorkDone += statValue;
+            if (sowWorkDone >= plant2.def.plant.sowWork)
+            {
+                SowPlant(actor, plant2);
+            }
         }
 
-        private void SowPlant(Plant lPlant, Pawn actor)
+        private void SowPlant(Pawn actor, Plant plantSowed)
         {
-            lPlant.Growth = 0.0001f;
-            Map.mapDrawer.MapMeshDirty(lPlant.Position, MapMeshFlag.Things);
+            plantSowed.Growth = 0.0001f;
+            Map.mapDrawer.MapMeshDirty(plantSowed.Position, MapMeshFlag.Things);
             actor.records.Increment(RecordDefOf.PlantsSown);
             Find.HistoryEventsManager.RecordEvent(new HistoryEvent(HistoryEventDefOf.SowedPlant,
                                                                    actor.Named(HistoryEventArgsNames.Doer)));
-            if (lPlant.def.plant.humanFoodPlant)
+            if (plantSowed.def.plant.humanFoodPlant)
                 Find.HistoryEventsManager.RecordEvent(new HistoryEvent(HistoryEventDefOf.SowedHumanFoodPlant,
                                                                        actor.Named(HistoryEventArgsNames.Doer)));
 
-            if (lPlant.def.IsMutantPlant())
+            if (plantSowed.IsMutantPlant())
             {
-                var newEv = new HistoryEvent(PMHistoryEventDefOf.SowMutagenicPlants, actor.Named(HistoryEventArgsNames.Doer),
-                                             lPlant.def.Named(HistoryEventArgsNames.Subject));
-                Find.HistoryEventsManager.RecordEvent(newEv);
+                var hEV = new HistoryEvent(PMHistoryEventDefOf.SowMutagenicPlants, actor.Named(HistoryEventArgsNames.Doer),
+                                           plantSowed.def.Named(HistoryEventArgsNames.Subject));
+                Find.HistoryEventsManager.RecordEvent(hEV);
             }
 
             ReadyForNextToil();
