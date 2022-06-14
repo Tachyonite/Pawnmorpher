c2f5cfcf841374da0f821a71c823c8aa47deb068
Zeracronius <zeracronius@outlook.dk>
2022-06-14 19:12:00 +0200

    Slight optimization on hediffdef immunization patch to remove allocation and dictionary resizing.

--- Source/Pawnmorphs/Esoteria/HPatches/HediffDefPatches.cs
+++ Source/Pawnmorphs/Esoteria/HPatches/HediffDefPatches.cs
@@ -16,28 +16,17 @@ namespace Pawnmorph.HPatches
     
     static class HediffDefPatches
     {
-        [NotNull] private static readonly Dictionary<HediffDef, bool> _immunityCache = new Dictionary<HediffDef, bool>();
+        [NotNull] private static readonly Dictionary<HediffDef, bool> _immunityCache = new Dictionary<HediffDef, bool>(1000);
 
         [HarmonyPatch(typeof(HediffDef), nameof(HediffDef.PossibleToDevelopImmunityNaturally))]
         private static class HediffImmunityPatch
         {
             private static bool Prefix(HediffDef __instance, ref bool __result)
             {
-                if (_immunityCache.TryGetValue(__instance, out bool chk))
-                {
-                    __result = chk;
+                if (_immunityCache.TryGetValue(__instance, out __result))
                     return false;
-                }
 
-
-                var hediffCompProperties_Immunizable = __instance.CompProps<HediffCompProperties_Immunizable>();
-                if (hediffCompProperties_Immunizable != null
-                 && (hediffCompProperties_Immunizable.immunityPerDayNotSick > 0f
-                  || hediffCompProperties_Immunizable.immunityPerDaySick > 0f))
-                    __result = true;
-                else
-                    __result = false;
-
+                __result = __instance.PossibleToDevelopImmunityNaturally();
                 _immunityCache[__instance] = __result;
                 return false;
             }
