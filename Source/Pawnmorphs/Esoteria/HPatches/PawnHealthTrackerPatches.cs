645e4f24a2eb40dfcf847777e300f02316dfd5da
Zeracronius <zeracronius@outlook.dk>
2022-09-15 13:34:08 +0200

    Fixed unnecesary private field accessor

--- Source/Pawnmorphs/Esoteria/HPatches/PawnHealthTrackerPatches.cs
+++ Source/Pawnmorphs/Esoteria/HPatches/PawnHealthTrackerPatches.cs
@@ -12,24 +12,12 @@ namespace Pawnmorph.HPatches
     [HarmonyPatch(typeof(Pawn_HealthTracker))]
     public static class PawnHealthTrackerPatches
     {
-        private static AccessTools.FieldRef<Pawn_HealthTracker, Pawn> _pawnFieldRef;
-        private static System.Reflection.FieldInfo _fieldInfo;
-
-        static PawnHealthTrackerPatches()
-        {
-            _pawnFieldRef = AccessTools.FieldRefAccess<Pawn_HealthTracker, Pawn>("pawn");
-            _fieldInfo = AccessTools.Field(typeof(Pawn_HealthTracker), "pawn");
-        }
-
-
         [HarmonyPatch(nameof(Pawn_HealthTracker.LethalDamageThreshold), MethodType.Getter), HarmonyPostfix]
-        static float LethalDamageThresholdPostfix(float __result, [NotNull] Pawn_HealthTracker __instance)
+        static float LethalDamageThresholdPostfix(float __result, [NotNull] Pawn ___pawn)
         {
-            Pawn pawn = _pawnFieldRef.Invoke(__instance);
-
-            if (pawn.RaceProps?.Humanlike == true)
+            if (___pawn.RaceProps?.Humanlike == true)
             {
-                return __result * pawn.BodySize;
+                return __result * ___pawn.BodySize;
             }
             
             return __result;
