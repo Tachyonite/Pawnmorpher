43a2e9df914e652a8ebb5b71c96b0a104d7f9a34
Zeracronius <zeracronius@outlook.dk>
2022-10-01 15:15:55 +0200

    Changed mutation thought worker to ignore mutations assigned to race by default. Closes #602

--- Source/Pawnmorphs/Esoteria/Thoughts/Worker_HasMutations.cs
+++ Source/Pawnmorphs/Esoteria/Thoughts/Worker_HasMutations.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Pawnmorph.DefExtensions;
+using Pawnmorph.Hediffs;
 using Pawnmorph.Hybrids;
 using RimWorld;
 using UnityEngine;
@@ -16,11 +17,6 @@ namespace Pawnmorph.Thoughts
     /// </summary>
     public class Worker_HasMutations : ThoughtWorker
     {
-
-        private List<Thought> _scratchList = new List<Thought>(); 
-
-
-
         /// <summary>
         /// return the thought state for the given pawn 
         /// </summary>
@@ -28,22 +24,33 @@ namespace Pawnmorph.Thoughts
         /// <returns></returns>
         protected override ThoughtState CurrentStateInternal(Pawn p)
         {
-            if (!def.IsValidFor(p)) return false;
+            if (!def.IsValidFor(p)) 
+                return ThoughtState.Inactive;
+
             MutationTracker mutTracker = p.GetMutationTracker();
-            if (mutTracker == null) return false;
+            if (mutTracker == null) 
+                return ThoughtState.Inactive;
             
-            if (!mutTracker.AllMutations.Any()) return false;
 
-            var morph = p.def.GetMorphOfRace();
-            var influence = morph == null
-                                ? MorphUtilities.GetMaxInfluenceOfRace(p.def)
-                                : morph.GetMaxInfluenceForBody(p.RaceProps.body);
+            if (!mutTracker.AllMutations.Any()) 
+                return ThoughtState.Inactive;
+            
+            var nInfluence = mutTracker.TotalNormalizedInfluence;
+            
+            RaceMutationSettingsExtension racialMutations = p.def.TryGetRaceMutationSettings();
+            if (racialMutations != null)
+            {
+                foreach (var racialMutationGiver in racialMutations.mutationRetrievers.OfType<Hediffs.MutationRetrievers.AnimalClassRetriever>())
+                    nInfluence -= mutTracker.GetDirectNormalizedInfluence(racialMutationGiver.animalClass);
+                
+            }
 
-            var nInfluence = mutTracker.TotalInfluence / influence;
-            var idx = Mathf.Clamp(nInfluence * def.stages.Count, 0, def.stages.Count - 1);
+            var idx = Mathf.FloorToInt(Mathf.Clamp(nInfluence * def.stages.Count, 0, def.stages.Count - 1));
 
-            return ThoughtState.ActiveAtStage(Mathf.FloorToInt(idx));  
+            if (idx > 0)
+                return ThoughtState.ActiveAtStage(idx);
 
+            return ThoughtState.Inactive;
             
         }
     }
