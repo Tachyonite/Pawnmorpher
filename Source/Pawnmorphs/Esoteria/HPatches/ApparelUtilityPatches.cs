663fb0a27992703511b08efb89c9c5e610b72c48
Zeracronius <zeracronius@outlook.dk>
2022-10-22 21:52:33 +0200

    Added harmony priority to replacement method.

--- Source/Pawnmorphs/Esoteria/HPatches/ApparelUtilityPatches.cs
+++ Source/Pawnmorphs/Esoteria/HPatches/ApparelUtilityPatches.cs
@@ -12,6 +12,7 @@ namespace Pawnmorph.HPatches
     [HarmonyPatch(typeof(ApparelUtility))]
     static class ApparelUtilityPatches
     {
+        [HarmonyPriority(Priority.Last)]
         [HarmonyPatch(nameof(ApparelUtility.HasPartsToWear)), HarmonyPrefix]
         static bool FixHasPartsFor(Pawn p, ThingDef apparel, ref bool __result) //vanilla function erroniously assumes if the pawn is not missing any body parts 
         {
