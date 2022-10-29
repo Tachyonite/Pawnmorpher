4848dc6ac63ae33085a72d0e9fefd30209a64abf
Zeracronius <zeracronius@outlook.dk>
2022-10-29 18:09:22 +0200

    Fixed missing designators

--- Source/Pawnmorphs/Esoteria/PawnmorphPatches.cs
+++ Source/Pawnmorphs/Esoteria/PawnmorphPatches.cs
@@ -294,6 +294,7 @@ namespace Pawnmorph
 
             //interaction patch
             methodsToPatch.Add(typeof(HealthCardUtility).GetMethod("CreateSurgeryBill", STATIC_FLAGS));
+            methodsToPatch.Add(AccessTools.Method(typeof(RimWorld.FloatMenuMakerMap), nameof(RimWorld.FloatMenuMakerMap.ChoicesAtFor)));
             PatchRescueMessage(methodsToPatch);
 
             methodsToPatch.Add(typeof(InteractionUtility).GetMethod(nameof(InteractionUtility.CanReceiveRandomInteraction), STATIC_FLAGS));
