57c0a1396b0052487bcfa99ad33dee60c956ee32
Pausbrak <rslego@gmail.com>
2023-02-02 01:18:11 -0500

    added doc I forgot to finish

--- Source/Pawnmorphs/Esoteria/HediffComp_Staged.cs
+++ Source/Pawnmorphs/Esoteria/HediffComp_Staged.cs
@@ -24,7 +24,7 @@ namespace Pawnmorph
         public ThoughtDef thought = null;
 
         /// <summary>
-        /// 
+        /// The hediff givers on this stage, if any
         /// </summary>
         public List<HediffGiver> hediffGivers;
 
