edf022a562db4788924b935de8cc496cae872a84
Zeracronius <zeracronius@outlook.dk>
2023-02-07 17:20:23 +0100

    Replaced spaces with tabs

--- Source/Pawnmorphs/Esoteria/UserInterface/Settings/Dialog_BlacklistAnimal.cs
+++ Source/Pawnmorphs/Esoteria/UserInterface/Settings/Dialog_BlacklistAnimal.cs
@@ -10,102 +10,102 @@ using Verse.Sound;
 
 namespace Pawnmorph.UserInterface.Settings
 {
-    internal class Dialog_BlacklistAnimal : Window
-    {
-        private static string HEADER_TEXT = "PMBlacklistFormerHumansHeader".Translate();
-        private static string DESCRIPTION_TEXT = "PMBlacklistFormerHumansText".Translate();
+	internal class Dialog_BlacklistAnimal : Window
+	{
+		private static string HEADER_TEXT = "PMBlacklistFormerHumansHeader".Translate();
+		private static string DESCRIPTION_TEXT = "PMBlacklistFormerHumansText".Translate();
 		private static string APPLY_BUTTON_TEXT = "ApplyButtonText".Translate();
-        private static string RESET_BUTTON_TEXT = "ResetButtonText".Translate();
-        private static string CANCEL_BUTTON_TEXT = "CancelButtonText".Translate();
-        private static string FAILED_PARSING_TEXT = "PMFailedToParse".Translate();
+		private static string RESET_BUTTON_TEXT = "ResetButtonText".Translate();
+		private static string CANCEL_BUTTON_TEXT = "CancelButtonText".Translate();
+		private static string FAILED_PARSING_TEXT = "PMFailedToParse".Translate();
 		private static Vector2 APPLY_BUTTON_SIZE = new Vector2(120f, 40f);
-        private static Vector2 RESET_BUTTON_SIZE = new Vector2(120f, 40f);
-        private static Vector2 CANCEL_BUTTON_SIZE = new Vector2(120f, 40f);
+		private static Vector2 RESET_BUTTON_SIZE = new Vector2(120f, 40f);
+		private static Vector2 CANCEL_BUTTON_SIZE = new Vector2(120f, 40f);
 		private static Vector2 RAW_BUTTON_SIZE = new Vector2(28, 28f);
 		private const float SPACER_SIZE = 17f;
 
-        private Dictionary<ThingDef, bool> _blacklistedAnimalDictonary;
-        List<string> _settingsReference;
-        FilterListBox<ThingDef> _animalListBox;
+		private Dictionary<ThingDef, bool> _blacklistedAnimalDictonary;
+		List<string> _settingsReference;
+		FilterListBox<ThingDef> _animalListBox;
 
-        public Dialog_BlacklistAnimal(List<string> settingsReference)
-        {
-            _settingsReference = settingsReference;
-            _blacklistedAnimalDictonary = new Dictionary<ThingDef, bool>();
-        }
+		public Dialog_BlacklistAnimal(List<string> settingsReference)
+		{
+			_settingsReference = settingsReference;
+			_blacklistedAnimalDictonary = new Dictionary<ThingDef, bool>();
+		}
 
-        public override void PostOpen()
-        {
-            base.PostOpen();
-            RefreshList();
-        }
+		public override void PostOpen()
+		{
+			base.PostOpen();
+			RefreshList();
+		}
 
-        private void RefreshList()
-        {
-            // Get all animal ThingDefs that are not dryads.
-            IEnumerable<ThingDef> animals = DefDatabase<ThingDef>.AllDefsListForReading.Where(x => x.race?.Animal == true && x.race?.Dryad != true);
-            animals = animals.Except(MorphDef.AllDefs.SelectMany(x => x.AllAssociatedAnimals));
+		private void RefreshList()
+		{
+			// Get all animal ThingDefs that are not dryads.
+			IEnumerable<ThingDef> animals = DefDatabase<ThingDef>.AllDefsListForReading.Where(x => x.race?.Animal == true && x.race?.Dryad != true);
+			animals = animals.Except(MorphDef.AllDefs.SelectMany(x => x.AllAssociatedAnimals));
 
-            _blacklistedAnimalDictonary = animals.ToDictionary(x => x, x => _settingsReference.Contains(x.defName) == false);
+			_blacklistedAnimalDictonary = animals.ToDictionary(x => x, x => _settingsReference.Contains(x.defName) == false);
 			
 
 			var filterList = new ListFilter<ThingDef>(animals, (animal, filterText) => animal.LabelCap.ToString().ToLower().Contains(filterText));
-            _animalListBox = new FilterListBox<ThingDef>(filterList);
-        }
+			_animalListBox = new FilterListBox<ThingDef>(filterList);
+		}
 
 
-        public override void DoWindowContents(Rect inRect)
-        {
-            float curY = 0;
-            Text.Font = GameFont.Medium;
-            Widgets.Label(new Rect(0, curY, inRect.width, Text.LineHeight), HEADER_TEXT);
+		public override void DoWindowContents(Rect inRect)
+		{
+			float curY = 0;
+			Text.Font = GameFont.Medium;
+			Widgets.Label(new Rect(0, curY, inRect.width, Text.LineHeight), HEADER_TEXT);
 
-            curY += Text.LineHeight;
+			curY += Text.LineHeight;
 
-            Text.Font = GameFont.Small;
-            Rect descriptionRect = new Rect(0, curY, inRect.width, 60);
-            Widgets.Label(descriptionRect, DESCRIPTION_TEXT);
+			Text.Font = GameFont.Small;
+			Rect descriptionRect = new Rect(0, curY, inRect.width, 60);
+			Widgets.Label(descriptionRect, DESCRIPTION_TEXT);
 
-            curY += descriptionRect.height;
+			curY += descriptionRect.height;
 			curY += 35;
 
 			float totalHeight = inRect.height - curY - Math.Max(APPLY_BUTTON_SIZE.y, Math.Max(RESET_BUTTON_SIZE.y, CANCEL_BUTTON_SIZE.y)) - SPACER_SIZE;
-            _animalListBox.Draw(inRect, 0, curY, totalHeight, (item, listing) =>
-            {
-                bool current = _blacklistedAnimalDictonary.TryGetValue(item, false);
-                listing.CheckboxLabeled(item.LabelCap, ref current, item.modContentPack.ModMetaData.Name);
-                _blacklistedAnimalDictonary[item] = current;
-            });
+			_animalListBox.Draw(inRect, 0, curY, totalHeight, (item, listing) =>
+			{
+				bool current = _blacklistedAnimalDictonary.TryGetValue(item, false);
+				listing.CheckboxLabeled(item.LabelCap, ref current, item.modContentPack.ModMetaData.Name);
+				_blacklistedAnimalDictonary[item] = current;
+			});
 
-            // Draw the apply, reset and cancel buttons.
+			// Draw the apply, reset and cancel buttons.
 			if (Widgets.ButtonImage(new Rect(5, inRect.height - 33, RAW_BUTTON_SIZE.x, RAW_BUTTON_SIZE.y), TexButton.Rename))
 			{
 				ShowRawInput();
 			}
 
-            float buttonVertPos = inRect.height - Math.Max(APPLY_BUTTON_SIZE.y, Math.Max(RESET_BUTTON_SIZE.y, CANCEL_BUTTON_SIZE.y));
-            float applyHorPos = inRect.width / 2 - APPLY_BUTTON_SIZE.x - RESET_BUTTON_SIZE.x / 2 - SPACER_SIZE;
-            float resetHorPos = inRect.width / 2 - RESET_BUTTON_SIZE.x / 2;
-            float cancelHorPos = inRect.width / 2 + RESET_BUTTON_SIZE.x / 2 + SPACER_SIZE;
+			float buttonVertPos = inRect.height - Math.Max(APPLY_BUTTON_SIZE.y, Math.Max(RESET_BUTTON_SIZE.y, CANCEL_BUTTON_SIZE.y));
+			float applyHorPos = inRect.width / 2 - APPLY_BUTTON_SIZE.x - RESET_BUTTON_SIZE.x / 2 - SPACER_SIZE;
+			float resetHorPos = inRect.width / 2 - RESET_BUTTON_SIZE.x / 2;
+			float cancelHorPos = inRect.width / 2 + RESET_BUTTON_SIZE.x / 2 + SPACER_SIZE;
 			if (Widgets.ButtonText(new Rect(applyHorPos, buttonVertPos, APPLY_BUTTON_SIZE.x, APPLY_BUTTON_SIZE.y), APPLY_BUTTON_TEXT))
-            {
-                OnAcceptKeyPressed();
-            }
-            if (Widgets.ButtonText(new Rect(resetHorPos, buttonVertPos, RESET_BUTTON_SIZE.x, RESET_BUTTON_SIZE.y), RESET_BUTTON_TEXT))
-            {
-                RimWorld.SoundDefOf.Checkbox_TurnedOff.PlayOneShotOnCamera(null);
-                RefreshList();
-            }
-            if (Widgets.ButtonText(new Rect(cancelHorPos, buttonVertPos, CANCEL_BUTTON_SIZE.x, CANCEL_BUTTON_SIZE.y), CANCEL_BUTTON_TEXT))
-            {
-                OnCancelKeyPressed();
-            }
-        }
+			{
+				OnAcceptKeyPressed();
+			}
+			if (Widgets.ButtonText(new Rect(resetHorPos, buttonVertPos, RESET_BUTTON_SIZE.x, RESET_BUTTON_SIZE.y), RESET_BUTTON_TEXT))
+			{
+				RimWorld.SoundDefOf.Checkbox_TurnedOff.PlayOneShotOnCamera(null);
+				RefreshList();
+			}
+			if (Widgets.ButtonText(new Rect(cancelHorPos, buttonVertPos, CANCEL_BUTTON_SIZE.x, CANCEL_BUTTON_SIZE.y), CANCEL_BUTTON_TEXT))
+			{
+				OnCancelKeyPressed();
+			}
+		}
 
 
 		private void ShowRawInput()
 		{
-            List<string> exportCollection = new List<string>();
+			List<string> exportCollection = new List<string>();
 			exportCollection.AddRange(_blacklistedAnimalDictonary.Where(x => x.Value == false).Select(x => x.Key.defName).ToList());
 			exportCollection = exportCollection.Distinct().ToList();
 
@@ -116,9 +116,9 @@ namespace Pawnmorph.UserInterface.Settings
 			{
 				try
 				{
-                    _settingsReference.Clear();
+					_settingsReference.Clear();
 					_settingsReference.AddRange(value.Split(',').Where(x => String.IsNullOrWhiteSpace(x) == false).Distinct());
-                    RefreshList();
+					RefreshList();
 				}
 				catch (Exception)
 				{
@@ -130,48 +130,48 @@ namespace Pawnmorph.UserInterface.Settings
 		}
 
 		private void ApplyChanges()
-        {
-            // We want to persist any definitions assigned for defnames that could not be loaded into the game at present time.
-            // Like animals from currently disabled mods.
-            // Take the old list, add all disabled animals (this will cause duplicates) and then apply distinct.
+		{
+			// We want to persist any definitions assigned for defnames that could not be loaded into the game at present time.
+			// Like animals from currently disabled mods.
+			// Take the old list, add all disabled animals (this will cause duplicates) and then apply distinct.
 			_settingsReference.AddRange(_blacklistedAnimalDictonary.Where(x => x.Value == false).Select(x => x.Key.defName).ToList());
-            _settingsReference = _settingsReference.Distinct().ToList();
+			_settingsReference = _settingsReference.Distinct().ToList();
 
-            foreach (var animal in _blacklistedAnimalDictonary)
-            {
-                FormerHumanSettings formerHumanConfig = animal.Key.GetModExtension<FormerHumanSettings>();
+			foreach (var animal in _blacklistedAnimalDictonary)
+			{
+				FormerHumanSettings formerHumanConfig = animal.Key.GetModExtension<FormerHumanSettings>();
 
-                if (animal.Value == false)
+				if (animal.Value == false)
 				{
 					if (formerHumanConfig == null)
 					{
 						formerHumanConfig = new FormerHumanSettings();
-                        if (animal.Key.modExtensions == null)
-                            animal.Key.modExtensions = new List<DefModExtension>();
+						if (animal.Key.modExtensions == null)
+							animal.Key.modExtensions = new List<DefModExtension>();
 
 						animal.Key.modExtensions.Add(formerHumanConfig);
 					}
 
 					formerHumanConfig.neverFormerHuman = true;
 				} 
-                else if (formerHumanConfig != null)
-                {
-                    // Enable animal as valid former human
-                    formerHumanConfig.neverFormerHuman = false;
-                }
+				else if (formerHumanConfig != null)
+				{
+					// Enable animal as valid former human
+					formerHumanConfig.neverFormerHuman = false;
+				}
 			}
 		}
 
-        public override void OnCancelKeyPressed()
-        {
-            base.OnCancelKeyPressed();
-        }
+		public override void OnCancelKeyPressed()
+		{
+			base.OnCancelKeyPressed();
+		}
 
-        public override void OnAcceptKeyPressed()
-        {
-            ApplyChanges();
+		public override void OnAcceptKeyPressed()
+		{
+			ApplyChanges();
 
-            base.OnAcceptKeyPressed();
-        }
-    }
+			base.OnAcceptKeyPressed();
+		}
+	}
 }
