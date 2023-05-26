b40d023b4c70465f5ab909927f023b250d8203e2
Zeracronius <zeracronius@outlook.dk>
2023-05-26 21:50:41 +0200

    Added sequencer settings

--- Source/Pawnmorphs/Esoteria/PawnmorpherMod.cs
+++ Source/Pawnmorphs/Esoteria/PawnmorpherMod.cs
@@ -124,6 +124,13 @@ namespace Pawnmorph
 
 			coreNode.AddChild(nameof(PawnmorpherSettings.hostileKeepFactionTfChance), null, (in Rect x) => Widgets.HorizontalSlider(x, ref settings.hostileKeepFactionTfChance, new FloatRange(0, 1), settings.hostileKeepFactionTfChance.ToStringByStyle(ToStringStyle.PercentOne)), true);
 
+
+
+
+			TreeNode_FilterBox sequencerNode = coreNode.AddChild("PMSequencer");
+			sequencerNode.AddChild("PMSequencerMultipler", "PMSequencerMultiplerTooltip", callback: (in Rect x) => Widgets.HorizontalSlider(x, ref settings.SequencingMultiplier, new FloatRange(0.1f, 5), settings.SequencingMultiplier.ToStringPercent(), 0.1f));
+			sequencerNode.AddChild("PMSequencerAutoAnimalGenome", "PMSequencerAutoAnimalGenomeTooltip", callback: (in Rect x) => Widgets.Checkbox(x.position, ref settings.AutoSequenceAnimalGenome));
+
 			result.Add(coreNode);
 
 
