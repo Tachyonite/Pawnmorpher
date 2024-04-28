0082ba9a95fd1f3a443b888b2650b6a814b2dae1
Zeracronius <zeracronius@outlook.dk>
2024-04-28 11:58:25 +0200

    Fixed #903

--- Source/Pawnmorphs/Esoteria/HPatches/Optional/FoodStackMultiplier.cs
+++ Source/Pawnmorphs/Esoteria/HPatches/Optional/FoodStackMultiplier.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Security.Cryptography;
 using HarmonyLib;
 using RimWorld;
 using UnityEngine;
@@ -26,11 +27,13 @@ namespace Pawnmorph.HPatches.Optional
 		[HarmonyPatch(typeof(FoodUtility), nameof(FoodUtility.WillIngestStackCountOf)), HarmonyPrefix]
 		static bool WillIngestStackCountOf(Pawn ingester, ThingDef def, float singleFoodNutrition, ref int __result)
 		{
-			float num = Mathf.Min(def.ingestible.maxNumToIngestAtOnce * ingester.BodySize, FoodUtility.StackCountForNutrition(ingester.needs.food.NutritionWanted, singleFoodNutrition));
-			if (num < 1)
-			{
-				num = 1;
-			}
+			int maxIngestOnce = def.ingestible.maxNumToIngestAtOnce;
+
+			float num = FoodUtility.StackCountForNutrition(ingester.needs.food.NutritionWanted, singleFoodNutrition);
+			if (maxIngestOnce > 0)
+				num = Mathf.Min(maxIngestOnce * ingester.BodySize, num);
+
+			num = Mathf.Max(num, 1);
 			__result = (int)num;
 			return false;
 		}
@@ -43,11 +46,17 @@ namespace Pawnmorph.HPatches.Optional
 		[HarmonyPatch(typeof(Thing), "IngestedCalculateAmounts"), HarmonyPrefix]
 		static bool IngestedCalculateAmounts(Pawn ingester, float nutritionWanted, out int numTaken, out float nutritionIngested, Thing __instance, ThingDef ___def, int ___stackCount)
 		{
+			int maxIngestOnce = ___def.ingestible.maxNumToIngestAtOnce;
+
 			float nutrition = __instance.GetStatValue(StatDefOf.Nutrition);
 			numTaken = Mathf.CeilToInt(nutritionWanted / nutrition);
-			numTaken = (int)Mathf.Min(numTaken, ___def.ingestible.maxNumToIngestAtOnce * ingester.BodySize, ___stackCount);
+			numTaken = Mathf.Min(numTaken, ___stackCount);
+			
+			if (maxIngestOnce > 0)
+				numTaken = (int)Mathf.Min(numTaken, maxIngestOnce * ingester.BodySize);
+
 			numTaken = Mathf.Max(numTaken, 1);
-			nutritionIngested = (float)numTaken * nutrition;
+			nutritionIngested = numTaken * nutrition;
 
 			return false;
 		}
